d3f2018224feb5bc84afc0109e98c8942a5478aa
Sven van Haastregt <sven.vanhaastregt@arm.com>
2019-01-07 11:24:23 +0000

    Set noduplicate attribute for barriers
    Without this attribute, LLVM passes such as SimplifyCFG may duplicate
    barrier calls, which would cause incorrect behaviour of the compiled
    program.

--- lib/SPIRV/SPIRVReader.cpp
+++ lib/SPIRV/SPIRVReader.cpp
@@ -2527,6 +2527,7 @@ CallInst *SPIRVToLLVM::transOCLBarrier(BasicBlock *BB, SPIRVWord ExecScope,
     Func->setCallingConv(CallingConv::SPIR_FUNC);
     if (isFuncNoUnwind())
       Func->addFnAttr(Attribute::NoUnwind);
+    Func->addFnAttr(Attribute::NoDuplicate);
   }
 
   return CallInst::Create(Func, Arg, "", BB);
