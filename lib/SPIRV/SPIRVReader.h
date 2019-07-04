08af51245ce409bb42fa761bb1695d5989e6c046
Andrzej Ratajewski <andrzej.ratajewski@intel.com>
2019-07-04 10:30:03 +0200

    Move OpControlBarrier and OpMemoryBarrier translation into SPIRVToOCL passes. Their translation logic is dependent on source language version, so it should be seperated between version-dependent passes.

--- lib/SPIRV/SPIRVReader.h
+++ lib/SPIRV/SPIRVReader.h
@@ -112,7 +112,6 @@ public:
                                     SPIRVInstruction *BI, BasicBlock *BB);
   Instruction *transOCLBuiltinFromInst(SPIRVInstruction *BI, BasicBlock *BB);
   Instruction *transSPIRVBuiltinFromInst(SPIRVInstruction *BI, BasicBlock *BB);
-  Instruction *transOCLBarrierFence(SPIRVInstruction *BI, BasicBlock *BB);
   void transOCLVectorLoadStore(std::string &UnmangledName,
                                std::vector<SPIRVWord> &BArgs);
 
@@ -258,11 +257,6 @@ private:
   Instruction *transOCLAllAny(SPIRVInstruction *BI, BasicBlock *BB);
   Instruction *transOCLRelational(SPIRVInstruction *BI, BasicBlock *BB);
 
-  CallInst *transOCLBarrier(BasicBlock *BB, SPIRVWord ExecScope,
-                            SPIRVWord MemSema, SPIRVWord MemScope);
-
-  CallInst *transOCLMemFence(BasicBlock *BB, SPIRVWord MemSema,
-                             SPIRVWord MemScope);
 }; // class SPIRVToLLVM
 
 } // namespace SPIRV
