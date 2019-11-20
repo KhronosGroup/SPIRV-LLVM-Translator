64b30bf48e01da102eafacedac71f9179db9a9a3
Andrzej Ratajewski <andrzej.ratajewski@intel.com>
2019-11-20 11:07:54 +0100

    Translation of SPV_INTEL_media_block_io from SPIRV to LLVM

--- lib/SPIRV/SPIRVToOCL.cpp
+++ lib/SPIRV/SPIRVToOCL.cpp
@@ -97,6 +97,10 @@ void SPIRVToOCL::visitCallInst(CallInst &CI) {
     visitCallSPIRVPipeBuiltin(&CI, OC);
     return;
   }
+  if (isMediaBlockINTELOpcode(OC)) {
+    visitCallSPIRVImageMediaBlockBuiltin(&CI, OC);
+    return;
+  }
   if (OCLSPIRVBuiltinMap::rfind(OC))
     visitCallSPIRVBuiltin(&CI, OC);
 }
@@ -316,6 +320,41 @@ void SPIRVToOCL::visitCallSPIRVPipeBuiltin(CallInst *CI, Op OC) {
           P = CastInst::CreatePointerBitCastOrAddrSpaceCast(P, NewTy, "", CI);
         }
         return DemangledName;
+      },
+      &Attrs);
+}
+
+void SPIRVToOCL::visitCallSPIRVImageMediaBlockBuiltin(CallInst *CI, Op OC) {
+  AttributeList Attrs = CI->getCalledFunction()->getAttributes();
+  mutateCallInstOCL(
+      M, CI,
+      [=](CallInst *, std::vector<Value *> &Args) {
+        // Moving the first argument to the end.
+        std::rotate(Args.rbegin(), Args.rend() - 1, Args.rend());
+        Type *FuncType = CI->getType();
+        if (OC == OpSubgroupImageMediaBlockWriteINTEL) {
+          assert(Args.size() >= 4 && "Wrong media block write signature");
+          FuncType = Args.at(3)->getType(); // texel type
+        }
+        unsigned int BitWidth = FuncType->getScalarSizeInBits();
+        std::string FuncPostfix;
+        if (BitWidth == 8)
+          FuncPostfix = "_uc";
+        else if (BitWidth == 16)
+          FuncPostfix = "_us";
+        else if (BitWidth == 32)
+          FuncPostfix = "_ui";
+        else
+          assert(0 && "Unsupported texel type!");
+
+        if (FuncType->isVectorTy()) {
+          unsigned int NumEl = FuncType->getVectorNumElements();
+          assert((NumEl == 2 || NumEl == 4 || NumEl == 8 || NumEl == 16) &&
+                 "Wrong function type!");
+          FuncPostfix += std::to_string(NumEl);
+        }
+
+        return OCLSPIRVBuiltinMap::rmap(OC) + FuncPostfix;
       },
       &Attrs);
 }
