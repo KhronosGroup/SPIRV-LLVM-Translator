91bb8c3891a5364dfff661c469cb021aa0cefeef
Ben Ashbaugh <ben.ashbaugh@intel.com>
2020-03-23 18:06:06 -0700

    added cl_khr_subgroup_non_uniform_arithmetic

--- lib/SPIRV/OCL20ToSPIRV.cpp
+++ lib/SPIRV/OCL20ToSPIRV.cpp
@@ -955,14 +955,19 @@ void OCL20ToSPIRV::visitCallGroupBuiltin(CallInst *CI,
             return true; // continue
           PreOps.push_back(G);
           StringRef Op = StringSwitch<StringRef>(FuncName)
-              .StartsWith("ballot", "group_ballot_bit_count_")
-              .StartsWith("non_uniform", kSPIRVName::GroupNonUniformPrefix)
-              .Default(kSPIRVName::GroupPrefix);
+            .StartsWith("ballot", "group_ballot_bit_count_")
+            .StartsWith("non_uniform", kSPIRVName::GroupNonUniformPrefix)
+            .Default(kSPIRVName::GroupPrefix);
+          StringRef LogicalOp =
+            FuncName.contains("logical_") ?
+            "logical_" : "";
           StringRef GroupOp = StringSwitch<StringRef>(FuncName)
-              .Case("ballot_bit_count", "add")
-              .Case("ballot_inclusive_scan", "add")
-              .Case("ballot_exclusive_scan", "add")
-              .Default(FuncName.take_back(3));   // assumes op is three characters
+            .Case("ballot_bit_count", "add")
+            .Case("ballot_inclusive_scan", "add")
+            .Case("ballot_exclusive_scan", "add")
+            .Default(FuncName.take_back(3));    // assumes op is three characters
+          if (GroupOp.startswith("_"))
+            GroupOp = GroupOp.take_back(2);     // when op is two characters
           assert(!GroupOp.empty() && "Invalid OpenCL group builtin function");
           char OpTyC = 0;
           auto NeedSign = GroupOp == "max" || GroupOp == "min";
@@ -981,7 +986,7 @@ void OCL20ToSPIRV::visitCallGroupBuiltin(CallInst *CI,
           } else
             llvm_unreachable("Invalid OpenCL group builtin argument type");
 
-          DemangledName = Op.str() + OpTyC + GroupOp.str();
+          DemangledName = Op.str() + LogicalOp.str() + OpTyC + GroupOp.str();
           return false; // break out of loop
         });
   }
