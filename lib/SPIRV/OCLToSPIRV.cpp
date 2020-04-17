a3280a1116aca0236b338e48ccc67406da8f14ea
Andrzej Ratajewski <andrzej.ratajewski@intel.com>
2020-04-17 09:56:17 +0200

    Remove duplicated values from OCLSPIRVBuiltinMap.

--- lib/SPIRV/OCLToSPIRV.cpp
+++ lib/SPIRV/OCLToSPIRV.cpp
@@ -940,6 +940,7 @@ void OCLToSPIRV::visitCallGroupBuiltin(CallInst *CI,
           if (!FuncName.startswith(S))
             return true; // continue
           PreOps.push_back(G);
+<<<<<<< HEAD
           StringRef Op =
               StringSwitch<StringRef>(FuncName)
                   .StartsWith("ballot", "group_ballot_bit_count_")
@@ -949,6 +950,18 @@ void OCLToSPIRV::visitCallGroupBuiltin(CallInst *CI,
           StringRef ClusteredOp =
               FuncName.contains("clustered_") ? "non_uniform_" : "";
           StringRef LogicalOp = FuncName.contains("logical_") ? "logical_" : "";
+=======
+          StringRef Op = StringSwitch<StringRef>(FuncName)
+            .StartsWith("ballot", "group_ballot_bit_count_")
+            .StartsWith("non_uniform", kSPIRVName::GroupNonUniformPrefix)
+            .Default(kSPIRVName::GroupPrefix);
+          // clustered functions are handled with non uniform group opcodes
+          StringRef ClusteredOp =
+              FuncName.contains("clustered_") ? "non_uniform_" : "";
+          StringRef LogicalOp =
+            FuncName.contains("logical_") ?
+            "logical_" : "";
+>>>>>>> Remove duplicated values from OCLSPIRVBuiltinMap.
           StringRef GroupOp = StringSwitch<StringRef>(FuncName)
                                   .Case("ballot_bit_count", "add")
                                   .Case("ballot_inclusive_scan", "add")
