887326e76de620434cadd1e49b4a2f6630c2ee4b
Andrew Savonichev <andrew.savonichev@intel.com>
2020-05-25 12:59:09 +0300

    Fix a warning from "Enable strict rules to set ContractionOFF (#521)"
    Signed-off-by: Andrew Savonichev <andrew.savonichev@intel.com>

--- lib/SPIRV/SPIRVWriter.cpp
+++ lib/SPIRV/SPIRVWriter.cpp
@@ -2256,6 +2256,7 @@ bool LLVMToSPIRV::joinFPContract(Function *F, FPContract C) {
   case FPContract::DISABLED:
     return false;
   }
+  llvm_unreachable("Unhandled FPContract value.");
 }
 
 } // namespace SPIRV
