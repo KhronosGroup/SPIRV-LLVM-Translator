44aa37973be6964ef1bc9682f04d31a2d6b6ef04
nrudenko <nikita.rudenko@intel.com>
2020-07-09 16:42:01 +0300

    Add SIMTCallINTEL decoration of SPV_INTEL_vector_compute extension
    Extension is published at https://github.com/intel/llvm/pull/1612

--- lib/SPIRV/libSPIRV/SPIRVEnum.h
+++ lib/SPIRV/libSPIRV/SPIRVEnum.h
@@ -391,6 +391,7 @@ template <> inline void SPIRVMap<Decoration, SPIRVCapVec>::init() {
                {CapabilityVectorComputeINTEL});
   ADD_VEC_INIT(DecorationFuncParamIOKind, {CapabilityVectorComputeINTEL});
   ADD_VEC_INIT(DecorationStackCallINTEL, {CapabilityVectorComputeINTEL});
+  ADD_VEC_INIT(DecorationSIMTCallINTEL, {CapabilityVectorComputeINTEL});
 }
 
 template <> inline void SPIRVMap<BuiltIn, SPIRVCapVec>::init() {
