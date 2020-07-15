dc1d1fb63aa5a20739da2ba9fbb5c26b9f0d4b22
nrudenko <nikita.rudenko@intel.com>
2020-07-15 15:49:03 +0300

    [Backport to 14] Update SPV_INTEL_vector_compute to rev 15
    This adds NamedBarrierCountINTEL Execution Mode,
    see more in https://github.com/intel/llvm/pull/1612

--- lib/SPIRV/SPIRVReader.cpp
+++ lib/SPIRV/SPIRVReader.cpp
@@ -4171,6 +4171,13 @@ bool SPIRVToLLVM::transVectorComputeMetadata(SPIRVFunction *BF) {
     F->addFnAttr(Attr);
   }
 
+  if (auto *EM = BF->getExecutionMode(ExecutionModeNamedBarrierCountINTEL)) {
+    unsigned int NBarrierCnt = EM->getLiterals()[0];
+    Attribute Attr = Attribute::get(*Context, kVCMetadata::VCNamedBarrierCount,
+                                    std::to_string(NBarrierCnt));
+    F->addFnAttr(Attr);
+  }
+
   return true;
 }
 
