66cfa9772237e9c39c07e457fc20efefc6825014
Alexey Sachkov <alexey.sachkov@intel.com>
2020-10-02 18:56:25 +0300

    Remove OCL21ToSPIRV pass
    Since
    KhronosGroup/SPIRV-LLVM-Translator@6ceea975375b44fe0238fb30f6dffc50a9c33fb5
    this pass only handles input files coming out OpenCL C++ 2.1, which
    technically doesn't exists and therefore this should be a dead code.
    
    Additionally: most of the pass functionality (except sub-group barrier
    handling) is untested and duplicated in OCLToSPIRV pass.

--- lib/SPIRV/SPIRVWriter.cpp
+++ lib/SPIRV/SPIRVWriter.cpp
@@ -3583,7 +3583,6 @@ void addPassesForSPIRV(legacy::PassManager &PassMgr,
   if (Opts.isSPIRVMemToRegEnabled())
     PassMgr.add(createPromoteMemoryToRegisterPass());
   PassMgr.add(createPreprocessMetadata());
-  PassMgr.add(createOCL21ToSPIRV());
   PassMgr.add(createSPIRVLowerSPIRBlocks());
   PassMgr.add(createOCLTypeToSPIRV());
   PassMgr.add(createSPIRVLowerOCLBlocks());
