69e85029a52d54f1469c2bbe7891aa93b21c41c4
Dmitry Sidorov <dmitry.sidorov@intel.com>
2020-11-20 20:14:09 +0300

    Add intel::nofusion loop attribute translation
    The new loop control bit NoFusionINTEL is enabled by the FPGALoopControlsINTEL
    capability. If this bit is set, it indicates that the loop should not be fused
    with any adjacent loop.
    
    Signed-off-by: Dmitry Sidorov <dmitry.sidorov@intel.com>

--- lib/SPIRV/SPIRVReader.cpp
+++ lib/SPIRV/SPIRVReader.cpp
@@ -959,6 +959,8 @@ void SPIRVToLLVM::setLLVMLoopMetadata(const LoopInstType *LM,
     assert(NumParam <= LoopControlParameters.size() &&
            "Missing loop control parameter!");
   }
+  if (LC & LoopControlNoFusionINTELMask)
+    Metadata.push_back(getMetadataFromName("llvm.loop.fusion.disable"));
   llvm::MDNode *Node = llvm::MDNode::get(*Context, Metadata);
 
   // Set the first operand to refer itself
