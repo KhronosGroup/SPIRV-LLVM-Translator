07bcded23ebcbd0b11b1567bfd0bbfda0bd4a522
Artem Gindinson <artem.gindinson@intel.com>
2020-11-23 18:49:19 +0300

    Support the SPV_EXT_shader_atomic_float_add extension
    The new `AtomicFAddEXT` instruction will be mapped onto
    `__spirv_AtomicFAddEXT()` external calls in LLVM IR. No
    additional logic is required to facilitate this - the
    existing infrastructure does the replacement by default
    based on the `__spirv` prefix.
    
    The full specification can be found at
    github.com/KhronosGroup/SPIRV-Registry/blob/master/extensions/EXT/SPV_EXT_shader_atomic_float_add.asciidoc
    
    Signed-off-by: Artem Gindinson <artem.gindinson@intel.com>

--- lib/SPIRV/libSPIRV/spirv.hpp
+++ lib/SPIRV/libSPIRV/spirv.hpp
@@ -968,6 +968,8 @@ enum Capability {
   CapabilityFPGALoopControlsINTEL = 5888,
   CapabilityBlockingPipesINTEL = 5945,
   CapabilityFPGARegINTEL = 5948,
+  CapabilityAtomicFloat32AddEXT = 6033,
+  CapabilityAtomicFloat64AddEXT = 6034,
   CapabilityFastCompositeINTEL = 6093,
   CapabilityOptNoneINTEL = 6094,
   CapabilityMax = 0x7fffffff,
@@ -1508,6 +1510,7 @@ enum Op {
   OpReadPipeBlockingINTEL = 5946,
   OpWritePipeBlockingINTEL = 5947,
   OpFPGARegINTEL = 5949,
+  OpAtomicFAddEXT = 6035,
   OpTypeBufferSurfaceINTEL = 6086,
   OpMax = 0x7fffffff,
 };
@@ -1908,6 +1911,7 @@ inline void HasResultAndType(Op opcode, bool *hasResult, bool *hasResultType) {
     case OpSubgroupImageMediaBlockWriteINTEL: *hasResult = false; *hasResultType = false; break;
     case OpUCountLeadingZerosINTEL: *hasResult = true; *hasResultType = true; break;
     case OpUCountTrailingZerosINTEL: *hasResult = true; *hasResultType = true; break;
+    case OpAtomicFAddEXT: *hasResult = true; *hasResultType = true; break;
     case OpAbsISubINTEL: *hasResult = true; *hasResultType = true; break;
     case OpAbsUSubINTEL: *hasResult = true; *hasResultType = true; break;
     case OpIAddSatINTEL: *hasResult = true; *hasResultType = true; break;
