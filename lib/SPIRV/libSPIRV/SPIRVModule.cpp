4ebfce4bd4b3686abbab65fdca60e45b51e9e9e0
Dmitry <dmitry.sidorov@intel.com>
2020-12-01 13:42:52 +0300

    Move assume/expect to spirv_internal.hpp (#832)
    SPV_INTEL_optimization_hints isn't going to be upstreamed to SPIR-V
    Headers repository, therefore this patch is needed. Also it serves
    a purpose to show, how can extensions be implemented using
    spirv_internal.hpp.
    
    Signed-off-by: Dmitry Sidorov <dmitry.sidorov@intel.com>

--- lib/SPIRV/libSPIRV/SPIRVModule.cpp
+++ lib/SPIRV/libSPIRV/SPIRVModule.cpp
@@ -1488,7 +1488,7 @@ SPIRVInstruction *SPIRVModuleImpl::addExpectINTELInst(SPIRVType *ResultTy,
                                                       SPIRVValue *ExpectedValue,
                                                       SPIRVBasicBlock *BB) {
   return addInstruction(SPIRVInstTemplateBase::create(
-                            OpExpectINTEL, ResultTy, getId(),
+                            internal::OpExpectINTEL, ResultTy, getId(),
                             getVec(Value->getId(), ExpectedValue->getId()), BB,
                             this),
                         BB);
