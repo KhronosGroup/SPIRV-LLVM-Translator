a7094793465c921cc1a1933cdf7bd160a6c4473d
Viktoria Maksimova <viktoria.maksimova@intel.com>
2020-12-31 00:00:49 +0300

    Lower SIToFPInst with bool operand
    SIToFPInst LLVM instruction is translated to OpConvertSToF SPIRV instruction.
    OpConvertSToF instruction can not receive boolean operand, so replace it with
    the select instruction.

--- lib/SPIRV/SPIRVLowerBool.cpp
+++ lib/SPIRV/SPIRVLowerBool.cpp
@@ -110,6 +110,17 @@ public:
       I.setOperand(0, Sel);
     }
   }
+  virtual void visitSIToFPInst(SIToFPInst &I) {
+    auto Op = I.getOperand(0);
+    if (isBoolType(Op->getType())) {
+      auto Ty = Type::getInt32Ty(*Context);
+      auto Zero = getScalarOrVectorConstantInt(Ty, 0, false);
+      auto One = getScalarOrVectorConstantInt(Ty, 1, false);
+      assert(Zero && One && "Couldn't create constant int");
+      auto Sel = SelectInst::Create(Op, One, Zero, "", &I);
+      I.setOperand(0, Sel);
+    }
+  }
   bool runOnModule(Module &M) override {
     Context = &M.getContext();
     visit(M);
