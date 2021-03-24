81b0ebc9311b7dd83cd6fcb2bf889c9713f44aa9
Dmitry <dmitry.sidorov@intel.com>
2021-03-24 11:46:30 +0300

    Print path info in error messages only in debug build (#941)
    * Print path info in error messages only in debug build
    
    Signed-off-by: Dmitry Sidorov <dmitry.sidorov@intel.com>

--- lib/SPIRV/SPIRVUtil.cpp
+++ lib/SPIRV/SPIRVUtil.cpp
@@ -1543,9 +1543,8 @@ bool checkTypeForSPIRVExtendedInstLowering(IntrinsicInst *II, SPIRVModule *BM) {
     }
     if ((!Ty->isFloatTy() && !Ty->isDoubleTy() && !Ty->isHalfTy()) ||
         ((NumElems > 4) && (NumElems != 8) && (NumElems != 16))) {
-      BM->getErrorLog().checkError(false, SPIRVEC_InvalidFunctionCall,
-                                   II->getCalledOperand()->getName().str(), "",
-                                   __FILE__, __LINE__);
+      BM->SPIRVCK(
+          false, InvalidFunctionCall, II->getCalledOperand()->getName().str());
       return false;
     }
     break;
