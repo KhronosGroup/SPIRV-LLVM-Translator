9b2cc0c6b11ed43d4a57ea426cfdfb83bd078846
Dmitry Sidorov <dmitry.sidorov@intel.com>
2021-04-07 01:02:25 +0300

    Remove mapping of NoWrite function parameter attribute on qual metadata
    The NoWrite SPIR-V function parameter provides an assurance that
    memory pointed to by a NoWrite pointer will not be modified through
    that pointer.
    
    The const qualifier (represented in LLVM IR for kernel functions
    using kernel_arg_type_qual metadata) does not provide any such
    guarantee. There is nothing that prevents a function that takes a
    pointer to a const pointee from stripping the const pointer away
    and modifying the pointee.
    
    As such, it is not appropriate to translate from one to the other,
    and so the code for doing so should be removed, so that this
    translation is not performed either when translating from LLVM IR
    to SPIR-V, or when translating from SPIR-V to LLVM IR.
    
    Signed-off-by: Dmitry Sidorov <dmitry.sidorov@intel.com>

--- lib/SPIRV/SPIRVWriter.cpp
+++ lib/SPIRV/SPIRVWriter.cpp
@@ -3339,10 +3339,6 @@ bool LLVMToSPIRVBase::transOCLMetadata() {
               BA->addDecorate(
                   new SPIRVDecorate(DecorationFuncParamAttr, BA,
                                     FunctionParameterAttributeNoAlias));
-            if (Str.find("const") != std::string::npos)
-              BA->addDecorate(
-                  new SPIRVDecorate(DecorationFuncParamAttr, BA,
-                                    FunctionParameterAttributeNoWrite));
           });
     }
     if (auto *KernelArgName = F.getMetadata(SPIR_MD_KERNEL_ARG_NAME)) {
