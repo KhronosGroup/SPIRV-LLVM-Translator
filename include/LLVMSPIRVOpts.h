c7a4b59eb765b6ab6b937aa7323c73a4e0215b0e
Dmitry Sidorov <dmitry.sidorov@intel.com>
2021-04-07 00:39:14 +0300

    Fix licenses typos
    Signed-off-by: Dmitry Sidorov <dmitry.sidorov@intel.com>

--- include/LLVMSPIRVOpts.h
+++ include/LLVMSPIRVOpts.h
@@ -19,9 +19,9 @@
 // Redistributions in binary form must reproduce the above copyright notice,
 // this list of conditions and the following disclaimers in the documentation
 // and/or other materials provided with the distribution.
-// Neither the names of Advanced Micro Devices, Inc., nor the names of its
-// contributors may be used to endorse or promote products derived from this
-// Software without specific prior written permission.
+// Neither the names of Intel Corporation, nor the names of its contributors may
+// be used to endorse or promote products derived from this Software without
+// specific prior written permission.
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
--- tools/llvm-spirv/llvm-spirv.cpp
+++ tools/llvm-spirv/llvm-spirv.cpp
@@ -18,9 +18,6 @@
 // Redistributions in binary form must reproduce the above copyright notice,
 // this list of conditions and the following disclaimers in the documentation
 // and/or other materials provided with the distribution.
-// Neither the names of Advanced Micro Devices, Inc., nor the names of its
-// contributors may be used to endorse or promote products derived from this
-// Software without specific prior written permission.
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
