3286bd8fc394a504bc7c3df87578c32de4f7e996
Dmitry Sidorov <dmitry.sidorov@intel.com>
2021-04-07 15:06:57 +0300

    Promote SPV_INTEL_optimization_hints to SPV_KHR_expect_assume
    Signed-off-by: Dmitry Sidorov <dmitry.sidorov@intel.com>

--- lib/SPIRV/libSPIRV/SPIRVOpCodeEnumInternal.h
+++ lib/SPIRV/libSPIRV/SPIRVOpCodeEnumInternal.h
@@ -1,5 +1,3 @@
 #include "spirv_internal.hpp"
 
-_SPIRV_OP_INTERNAL(AssumeTrueINTEL, internal::OpAssumeTrueINTEL)
-_SPIRV_OP_INTERNAL(ExpectINTEL, internal::OpExpectINTEL)
 _SPIRV_OP_INTERNAL(Forward, internal::OpForward)
