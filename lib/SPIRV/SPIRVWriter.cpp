6276c575a26b3398b000a37b9d277e6e9e223544
Dmitry Sidorov <dmitry.sidorov@intel.com>
2021-04-15 20:14:47 +0300

    Fix clang-format
    Signed-off-by: Dmitry Sidorov <dmitry.sidorov@intel.com>

--- lib/SPIRV/SPIRVWriter.cpp
+++ lib/SPIRV/SPIRVWriter.cpp
@@ -2353,8 +2353,7 @@ SPIRVValue *LLVMToSPIRV::transIntrinsicInst(IntrinsicInst *II,
     // llvm.assume translation is currently supported only within
     // SPV_KHR_expect_assume extension, ignore it otherwise, since it's
     // an optimization hint
-    if (BM->isAllowedToUseExtension(
-            ExtensionID::SPV_KHR_expect_assume)) {
+    if (BM->isAllowedToUseExtension(ExtensionID::SPV_KHR_expect_assume)) {
       SPIRVValue *Condition = transValue(II->getArgOperand(0), BB);
       return BM->addAssumeTrueKHRInst(Condition, BB);
     }
@@ -2470,8 +2469,7 @@ SPIRVValue *LLVMToSPIRV::transIntrinsicInst(IntrinsicInst *II,
     // SPV_KHR_expect_assume extension, replace it with a translated value of #0
     // operand otherwise, since it's an optimization hint
     SPIRVValue *Value = transValue(II->getArgOperand(0), BB);
-    if (BM->isAllowedToUseExtension(
-            ExtensionID::SPV_KHR_expect_assume)) {
+    if (BM->isAllowedToUseExtension(ExtensionID::SPV_KHR_expect_assume)) {
       SPIRVType *Ty = transType(II->getType());
       SPIRVValue *ExpectedValue = transValue(II->getArgOperand(1), BB);
       return BM->addExpectKHRInst(Ty, Value, ExpectedValue, BB);
