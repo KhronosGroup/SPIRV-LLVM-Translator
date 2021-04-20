7edfae8284cc76ac3b648c110d81a581e78728ed
Andrzej Ratajewski <andrzej.ratajewski@intel.com>
2021-04-20 08:35:57 +0200

    Move OCL-IR translation logic related to EIS instructions to SPIRVToOCL pass
    This change also fixes vstore translation for SPV-IR generation path and adds
    test for vstore and printf.

--- lib/SPIRV/SPIRVInternal.h
+++ lib/SPIRV/SPIRVInternal.h
@@ -971,6 +971,10 @@ template <> inline void SPIRVMap<std::string, Op, SPIRVOpaqueType>::init() {
 // Check if the module contains llvm.loop.* metadata
 bool hasLoopMetadata(const Module *M);
 
+// Check if CI is a call to instruction from OpenCL Extended Instruction Set.
+// If so, return it's extended opcode in ExtOp.
+bool isSPIRVOCLExtInst(const CallInst *CI, OCLExtOpKind *ExtOp);
+
 } // namespace SPIRV
 
 #endif // SPIRV_SPIRVINTERNAL_H
