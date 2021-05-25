e303a5f8e9adc101fd89b5433cbda53bbfce330e
Dmitry Sidorov <dmitry.sidorov@intel.com>
2021-05-25 19:14:54 +0300

    Implement SPV_INTEL_fpga_invocation_pipelining_attributes extension
    Some FPGA devices and toolchains can support customizable levels or
    implementation of pipeline parallelism when mapping a SPIR-V module
    to hardware. Through pipeline parallelism, multiple invocations of
    a kernel or function can execute concurrently.
    
    This extension adds decorations to request that a kernel or function
    support invocations at a specified initiation interval, that multiple
    invocations are forbidden from executing concurrently, or that the
    kernel or function is limited to a maximum number of concurrent
    invocations.
    
    Spec:
    https://github.com/KhronosGroup/SPIRV-Registry/blob/38a7e56b9b7feab7fa8b3bea08770f7232b76ed2/extensions/INTEL/SPV_INTEL_fpga_invocation_pipelining_attributes.asciidoc
    
    Signed-off-by: Dmitry Sidorov <dmitry.sidorov@intel.com>

--- lib/SPIRV/libSPIRV/SPIRVEnum.h
+++ lib/SPIRV/libSPIRV/SPIRVEnum.h
@@ -429,6 +429,12 @@ template <> inline void SPIRVMap<Decoration, SPIRVCapVec>::init() {
                {internal::CapabilityMemoryAccessAliasingINTEL});
   ADD_VEC_INIT(internal::DecorationNoAliasINTEL,
                {internal::CapabilityMemoryAccessAliasingINTEL});
+  ADD_VEC_INIT(internal::DecorationInitiationIntervalINTEL,
+               {internal::CapabilityFPGAInvocationPipeliningAttributesINTEL});
+  ADD_VEC_INIT(internal::DecorationMaxConcurrencyINTEL,
+               {internal::CapabilityFPGAInvocationPipeliningAttributesINTEL});
+  ADD_VEC_INIT(internal::DecorationPipelineEnableINTEL,
+               {internal::CapabilityFPGAInvocationPipeliningAttributesINTEL});
 }
 
 template <> inline void SPIRVMap<BuiltIn, SPIRVCapVec>::init() {
