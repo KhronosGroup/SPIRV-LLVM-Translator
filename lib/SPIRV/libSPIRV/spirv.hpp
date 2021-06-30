be7fc383288b97a9b8ba199b8ad36f2cc293614a
Sven van Haastregt <sven.vanhaastregt@arm.com>
2021-06-30 17:09:05 +0100

    [NFC] Sync spirv.hpp and regenerate SPIRVNameMapEnum.h
    Bring spirv.hpp in sync with ddf3230 ("Merge pull request #222 from
    kpet/github-actions", 2021-06-30) from
    github.com/KhronosGroup/SPIRV-Headers .
    
    This brings the SPV_KHR_bit_instructions and SPV_INTEL_debug_module
    extension enum values.  Drop the internal enum value of the latter.

--- lib/SPIRV/libSPIRV/spirv.hpp
+++ lib/SPIRV/libSPIRV/spirv.hpp
@@ -1046,11 +1046,13 @@ enum Capability {
     CapabilityDotProductInput4x8BitKHR = 6017,
     CapabilityDotProductInput4x8BitPackedKHR = 6018,
     CapabilityDotProductKHR = 6019,
+    CapabilityBitInstructions = 6025,
     CapabilityAtomicFloat32AddEXT = 6033,
     CapabilityAtomicFloat64AddEXT = 6034,
     CapabilityLongConstantCompositeINTEL = 6089,
     CapabilityFastCompositeINTEL = 6093,
     CapabilityAtomicFloat16AddEXT = 6095,
+    CapabilityDebugInfoModuleINTEL = 6114,
     CapabilityMax = 0x7fffffff,
 };
 
