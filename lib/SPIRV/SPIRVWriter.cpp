3d574d1ce064121e3dfa0c16b2418bfd896f280a
Evgeny Naydanov <evgeny.naydanov@intel.com>
2021-08-17 11:35:29 +0300

    Fix leak in LLVMToSPIRV::transExecutionMode()
    In case spv::ExecutionModeSharedLocalMemorySizeINTEL the new
    SPIRVExecutionMode was not registered via SPIRVModule::add()
    and was leaking

--- lib/SPIRV/SPIRVWriter.cpp
+++ lib/SPIRV/SPIRVWriter.cpp
@@ -2746,8 +2746,8 @@ bool LLVMToSPIRV::transExecutionMode() {
           break;
         unsigned SLMSize;
         N.get(SLMSize);
-        BF->addExecutionMode(new SPIRVExecutionMode(
-            BF, static_cast<ExecutionMode>(EMode), SLMSize));
+        BF->addExecutionMode(BM->add(new SPIRVExecutionMode(
+            BF, static_cast<ExecutionMode>(EMode), SLMSize)));
       } break;
 
       case spv::ExecutionModeDenormPreserve:
