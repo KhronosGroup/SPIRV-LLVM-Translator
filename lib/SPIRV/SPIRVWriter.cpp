857bcdefbc48a2e7137930075069c0654245414c
Dmitry Sidorov <dmitry.sidorov@intel.com>
2021-09-06 16:24:55 +0300

    Don't try to apply aliasing decoration to SPIR-V instruction without return id (#1196)
    Signed-off-by: Dmitry Sidorov <dmitry.sidorov@intel.com>

--- lib/SPIRV/SPIRVWriter.cpp
+++ lib/SPIRV/SPIRVWriter.cpp
@@ -103,25 +103,6 @@ static void foreachKernelArgMD(
   }
 }
 
-static bool shouldTryToAddMemAliasingDecoration(Instruction *Inst) {
-  // Limit translation of aliasing metadata with only this set of instructions
-  // gracefully considering others as compilation mistakes and ignoring them
-  if (!Inst->mayReadOrWriteMemory())
-    return false;
-  // Loads and Stores are handled during memory access mask addition
-  if (isa<StoreInst>(Inst) || isa<LoadInst>(Inst))
-    return false;
-  CallInst *CI = dyn_cast<CallInst>(Inst);
-  if (!CI)
-    return true;
-  // Calls to intrinsics are skipped. At some point lifetime start/end will be
-  // handled separately, but specification isn't ready.
-  if (Function *Fun = CI->getCalledFunction())
-    if (Fun->isIntrinsic())
-      return false;
-  return true;
-}
-
 LLVMToSPIRV::LLVMToSPIRV(SPIRVModule *SMod)
     : ModulePass(ID), M(nullptr), Ctx(nullptr), BM(SMod), SrcLang(0),
       SrcLangVer(0) {
@@ -1511,6 +1492,31 @@ SPIRVValue *LLVMToSPIRV::mapValue(Value *V, SPIRVValue *BV) {
   ValueMap[V] = BV;
   SPIRVDBG(dbgs() << "[mapValue] " << *V << " => "; spvdbgs() << BV << "\n");
   return BV;
+}
+
+bool LLVMToSPIRV::shouldTryToAddMemAliasingDecoration(Instruction *Inst) {
+  // Limit translation of aliasing metadata with only this set of instructions
+  // gracefully considering others as compilation mistakes and ignoring them
+  if (!Inst->mayReadOrWriteMemory())
+    return false;
+  // Loads and Stores are handled during memory access mask addition
+  if (isa<StoreInst>(Inst) || isa<LoadInst>(Inst))
+    return false;
+  CallInst *CI = dyn_cast<CallInst>(Inst);
+  if (!CI)
+    return true;
+  if (Function *Fun = CI->getCalledFunction()) {
+    // Calls to intrinsics are skipped. At some point lifetime start/end will be
+    // handled separately, but specification isn't ready.
+    if (Fun->isIntrinsic())
+      return false;
+    // Also skip SPIR-V instructions that don't have result id to attach the
+    // decorations
+    if (isBuiltinTransToInst(Fun))
+      if (Fun->getReturnType()->isVoidTy())
+        return false;
+  }
+  return true;
 }
 
 bool LLVMToSPIRV::transDecoration(Value *V, SPIRVValue *BV) {
