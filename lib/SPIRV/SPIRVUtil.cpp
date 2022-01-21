e957fd70fab149ad686be644f192cfccc2917edc
Andrzej Ratajewski <andrzej.ratajewski@intel.com>
2022-01-21 17:59:51 +0200

    Fix SPIR-V Friendly IR for MultiReferenceInterlaced opcodes
    This change fixes mangling for the following opcodes:
    - `OpSubgroupAvcRefEvaluateWithMultiReferenceInterlacedINTEL`
    - `OpSubgroupAvcSicEvaluateWithMultiReferenceInterlacedINTEL`
    
    Spec: https://github.com/KhronosGroup/SPIRV-Registry/blob/main/extensions/INTEL/SPV_INTEL_device_side_avc_motion_estimation.asciidoc

--- lib/SPIRV/SPIRVUtil.cpp
+++ lib/SPIRV/SPIRVUtil.cpp
@@ -2086,6 +2086,8 @@ public:
     case OpSubgroupAvcImeGetStreamoutDualReferenceMajorShapeDistortionsINTEL:
     case OpSubgroupAvcImeGetStreamoutDualReferenceMajorShapeMotionVectorsINTEL:
     case OpSubgroupAvcImeGetStreamoutDualReferenceMajorShapeReferenceIdsINTEL:
+    case OpSubgroupAvcRefEvaluateWithMultiReferenceInterlacedINTEL:
+    case OpSubgroupAvcSicEvaluateWithMultiReferenceInterlacedINTEL:
     case OpSubgroupAvcRefEvaluateWithMultiReferenceINTEL:
     case OpSubgroupAvcSicEvaluateWithMultiReferenceINTEL:
       addUnsignedArgs(1, 2);
