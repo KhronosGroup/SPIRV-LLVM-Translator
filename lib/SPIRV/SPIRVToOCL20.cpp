9af3a5f660b9f29c1058585195372a1b0767fcca
Wenju He <wenju.he@intel.com>
2022-02-18 15:38:33 +0800

    Fix SPIRV -> OCL barrier call argument attributes (#1407)
    The first arg of CI is not used as arg in NewCI. So its attribute should
    be removed from attributes of NewCI.
    
    (cherry picked from commit f797de28e4f2d87e0f401faa453460f1e6e5a7ee)

--- lib/SPIRV/SPIRVToOCL20.cpp
+++ lib/SPIRV/SPIRVToOCL20.cpp
@@ -97,6 +97,10 @@ void SPIRVToOCL20Base::visitCallSPIRVMemoryBarrier(CallInst *CI) {
 
 void SPIRVToOCL20Base::visitCallSPIRVControlBarrier(CallInst *CI) {
   AttributeList Attrs = CI->getCalledFunction()->getAttributes();
+  SmallVector<AttributeSet, 2> ArgAttrs = {Attrs.getParamAttributes(1),
+                                           Attrs.getParamAttributes(2)};
+  AttributeList NewAttrs = AttributeList::get(
+      *Ctx, Attrs.getFnAttributes(), Attrs.getRetAttributes(), ArgAttrs);
   mutateCallInstOCL(
       M, CI,
       [=](CallInst *, std::vector<Value *> &Args) {
@@ -116,7 +120,7 @@ void SPIRVToOCL20Base::visitCallSPIRVControlBarrier(CallInst *CI) {
         return (ExecScope == ScopeWorkgroup) ? kOCLBuiltinName::WorkGroupBarrier
                                              : kOCLBuiltinName::SubGroupBarrier;
       },
-      &Attrs);
+      &NewAttrs);
 }
 
 std::string SPIRVToOCL20Base::mapFPAtomicName(Op OC) {
