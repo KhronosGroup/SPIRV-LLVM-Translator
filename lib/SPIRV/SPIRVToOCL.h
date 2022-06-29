c7c7da9e85188c899ed5ed022e6afe73fb9cd1f3
Sven van Haastregt <sven.vanhaastregt@arm.com>
2022-06-29 14:05:49 +0100

    [NFC] Use mutateArgsForImageOperands for SPIRVImageReadBuiltIn
    (cherry picked from commit 18d1f41f4d168bf3ea99263155e83350016bb6fe)

--- lib/SPIRV/SPIRVToOCL.h
+++ lib/SPIRV/SPIRVToOCL.h
@@ -269,8 +269,10 @@ public:
 
   std::string translateOpaqueType(StringRef STName);
 
-  /// Mutate the argument list based on (optional) image operands.
-  void mutateArgsForImageOperands(std::vector<Value *> &Args);
+  /// Mutate the argument list based on (optional) image operands at position
+  /// ImOpArgIndex.
+  void mutateArgsForImageOperands(std::vector<Value *> &Args,
+                                  unsigned ImOpArgIndex);
 
 protected:
   Module *M;
