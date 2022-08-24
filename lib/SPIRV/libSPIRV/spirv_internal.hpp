d4ec010c61654c920d0751bc894d4d8aae1b75f6
bowenxue-intel <bowen.xue@intel.com>
2022-08-24 08:50:53 -0700

    Implement SPIR-V support for max_reinvocation_delay attribute (#1594)
    The attribute generates the llvm.loop.intel.max_reinvocation_delay.count metadata in LLVM IR.
    There is one positive integer literal operand denoting the maximum number of cycles allowed
    between loop invocations.
    
    Spec: KhronosGroup/SPIRV-Registry#163

--- lib/SPIRV/libSPIRV/spirv_internal.hpp
+++ lib/SPIRV/libSPIRV/spirv_internal.hpp
@@ -86,7 +86,10 @@ enum InternalExecutionMode {
   IExecModeStreamingInterfaceINTEL = 6154
 };
 
-enum InternalLoopControlMask { ILoopControlLoopCountINTELMask = 0x1000000 };
+enum InternalLoopControlMask {
+  ILoopControlLoopCountINTELMask = 0x1000000,
+  ILoopControlMaxReinvocationDelayINTELMask = 0x2000000
+};
 
 constexpr LinkageType LinkageTypeInternal =
     static_cast<LinkageType>(ILTInternal);
@@ -180,6 +183,8 @@ constexpr ExecutionMode ExecutionModeStreamingInterfaceINTEL =
 
 static const LoopControlMask LoopControlLoopCountINTELMask =
     static_cast<LoopControlMask>(ILoopControlLoopCountINTELMask);
+static const LoopControlMask LoopControlMaxReinvocationDelayINTELMask =
+    static_cast<LoopControlMask>(ILoopControlMaxReinvocationDelayINTELMask);
 
 } // namespace internal
 } // namespace spv
