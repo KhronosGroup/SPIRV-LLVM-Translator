79b606fc8c1907cd9089c4360846238adb414b57
Dmitry Sidorov <dmitry.sidorov@intel.com>
2022-10-24 14:32:58 +0200

    [Backport to 10] Translate llvm.loop.unroll.full metadata
    It can be generated via #pragma clang unroll(full) pragma.
    llvm.loop.unroll.full means attempt to do full unroll of the
    loop and disable the unrolling if the trip count is not known
    at compile time.
    
    Unroll mask to which it was previously mapped doesn't much the
    description.
    
    The way the patch represents it in SPIR-V is:
     Unroll mask + PartialCount mask with '1' parameter
    
    This patch also removes some overtesting for unroll metadata.
    
    This backports: https://github.com/KhronosGroup/SPIRV-LLVM-Translator/pull/1664
    
    Signed-off-by: Sidorov, Dmitry <dmitry.sidorov@intel.com>

--- lib/SPIRV/SPIRVWriter.cpp
+++ lib/SPIRV/SPIRVWriter.cpp
@@ -972,8 +972,15 @@ getLoopControl(const BranchInst *Branch, std::vector<SPIRVWord> &Parameters,
       // appear first.
       if (S == "llvm.loop.unroll.disable")
         LoopControl |= spv::LoopControlDontUnrollMask;
-      else if (S == "llvm.loop.unroll.full" || S == "llvm.loop.unroll.enable")
+      else if (S == "llvm.loop.unroll.enable")
         LoopControl |= spv::LoopControlUnrollMask;
+      // Attempt to do full unroll of the loop and disable unrolling if the trip
+      // count is not known at compile time by setting PartialCount to 1
+      else if (S == "llvm.loop.unroll.full") {
+        LoopControl |= spv::LoopControlUnrollMask;
+        Parameters.push_back(1);
+        LoopControl |= spv::LoopControlPartialCountMask;
+      }
       // PartialCount must not be used with the DontUnroll bit
       else if (S == "llvm.loop.unroll.count" &&
                !(LoopControl & LoopControlDontUnrollMask)) {
