ea3ddc186d1e30ade5581c5c00a6b5ef9857655d
Dmitry Sidorov <dmitry.sidorov@intel.com>
2022-11-04 15:18:07 +0100

    Implement SPV_INTEL_tensor_float32_conversion extension (#1656)
    This extension adds conversion instruction from float to tensor float (TF32)
    data format. TF32 uses 1 bit for a sign, 8 bits for an exponent and 10 bits
    for a fraction. This extension doesn’t introduce TF32 type in SPIR-V, instead
    instruction below uses 32-bit float type to represent TF32 value.
    
    Spec: https://github.com/intel/llvm/pull/6990
    
    Signed-off-by: Sidorov, Dmitry <dmitry.sidorov@intel.com>

--- lib/SPIRV/libSPIRV/spirv_internal.hpp
+++ lib/SPIRV/libSPIRV/spirv_internal.hpp
@@ -46,6 +46,7 @@ enum InternalOp {
   IOpJointMatrixWorkItemLengthINTEL = 6410,
   IOpComplexFMulINTEL = 6415,
   IOpComplexFDivINTEL = 6416,
+  IOpConvertFToTF32INTEL = 6426,
   IOpMaskedGatherINTEL = 6428,
   IOpMaskedScatterINTEL = 6429,
   IOpPrev = OpMax - 2,
@@ -79,6 +80,7 @@ enum InternalCapability {
   ICapGlobalVariableDecorationsINTEL = 6146,
   ICapabilityNonConstantAddrspacePrintfINTEL = 6411,
   ICapabilityComplexFloatMulDivINTEL = 6414,
+  ICapabilityTensorFloat32ConversionINTEL = 6425,
   ICapabilityMaskedGatherScatterINTEL = 6427
 };
 
@@ -131,6 +133,9 @@ _SPIRV_OP(Op, ComplexFDivINTEL)
 _SPIRV_OP(Capability, MaskedGatherScatterINTEL)
 _SPIRV_OP(Op, MaskedGatherINTEL)
 _SPIRV_OP(Op, MaskedScatterINTEL)
+
+_SPIRV_OP(Capability, TensorFloat32ConversionINTEL)
+_SPIRV_OP(Op, ConvertFToTF32INTEL)
 #undef _SPIRV_OP
 
 constexpr Op OpForward = static_cast<Op>(IOpForward);
