b61f393713f609be5a5f1a1eac8eead4285ba783
Dmitry Sidorov <dmitry.sidorov@intel.com>
2022-11-08 10:40:39 +0100

    [Backport to 11] Translate readnone attribute as function parameter attribute (#1697)
    Community restricted readnone, readonly and writeonly attributes
    to be only function parameter attributes. This patch aligns
    the translator with llvm.org.
    
    It also fixes a bug, when readnone attribute is being mapped
    to NoWrite SPIR-V function parameter attribute.
    
    Signed-off-by: Sidorov, Dmitry <dmitry.sidorov@intel.com>
    
    Signed-off-by: Sidorov, Dmitry <dmitry.sidorov@intel.com>

--- lib/SPIRV/SPIRVWriter.cpp
+++ lib/SPIRV/SPIRVWriter.cpp
@@ -626,8 +626,10 @@ SPIRVFunction *LLVMToSPIRVBase::transFunctionDecl(Function *F) {
       BA->addAttr(FunctionParameterAttributeNoCapture);
     if (I->hasStructRetAttr())
       BA->addAttr(FunctionParameterAttributeSret);
-    if (I->onlyReadsMemory())
+    if (Attrs.hasParamAttr(ArgNo, Attribute::ReadOnly))
       BA->addAttr(FunctionParameterAttributeNoWrite);
+    if (Attrs.hasParamAttr(ArgNo, Attribute::ReadNone))
+      BA->addAttr(FunctionParameterAttributeNoReadWrite);
     if (Attrs.hasAttribute(ArgNo + 1, Attribute::ZExt))
       BA->addAttr(FunctionParameterAttributeZext);
     if (Attrs.hasAttribute(ArgNo + 1, Attribute::SExt))
