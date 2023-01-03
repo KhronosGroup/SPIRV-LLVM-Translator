63698eaeccf10de2c9d4dc8ee5e64420dbd00036
Sarnie, Nick <nick.sarnie@intel.com>
2023-01-03 15:06:53 -0500

    Translate RegisterAllocMode metadata
    Frontends can use the RegisterAllocMode metadata can be used to configure
    the register allocation mode. Here we translate into a form that
    Intel Graphics Compiler can use to actually apply the configuration.
    
    This is only a temporary solution, we intend to introduce
    an extension in the future
    
    Signed-off-by: Sarnie, Nick <nick.sarnie@intel.com>

--- lib/SPIRV/SPIRVWriter.cpp
+++ lib/SPIRV/SPIRVWriter.cpp
@@ -901,6 +901,27 @@ SPIRVFunction *LLVMToSPIRVBase::transFunctionDecl(Function *F) {
 
   transFPGAFunctionMetadata(BF, F);
 
+  if (auto RegisterAllocModeMD = F->getMetadata("RegisterAllocMode")) {
+    // TODO: Once the design for per-kernel register size allocation is
+    // finalized, we will need to move away from UserSemantic and introduce an
+    // extension
+    assert(isKernel(F) && "Only kernels should have RegisterAllocMode");
+    int RegisterAllocNodeMDOp = getMDOperandAsInt(RegisterAllocModeMD, 0);
+    // The current RegisterAllocMode metadata format is as follows
+    // AUTO - 0
+    // SMALL - 1
+    // LARGE - 2
+    // DEFAULT - 3
+    // Currently we only support SMALL and LARGE
+    if (RegisterAllocNodeMDOp == 1 || RegisterAllocNodeMDOp == 2) {
+      // 4 threads per eu means large grf mode, and 8 threads per eu
+      // means small grf mode
+      std::string NumThreads = RegisterAllocNodeMDOp == 2 ? "4" : "8";
+      BF->addDecorate(new SPIRVDecorateUserSemanticAttr(
+                          BF, "num-thread-per-eu " + NumThreads));
+    }
+  }
+
   SPIRVDBG(dbgs() << "[transFunction] " << *F << " => ";
            spvdbgs() << *BF << '\n';)
   return BF;
