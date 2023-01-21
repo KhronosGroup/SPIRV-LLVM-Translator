04a0e0e4cd25748366d1b27178527b6f7751f337
Viktoria Maximova <viktoria.maksimova@intel.com>
2023-01-21 21:45:36 +0100

    Deprecate SPV_INTEL_non_constant_addrspace_printf extension (#1818)
    This change continues #1749.
    We are removing SPV_INTEL_non_constant_addrspace_printf extension in
    favor of SPV_EXT_relaxed_printf_string_address_space, which are
    basically the same.
    
    Signed-off-by: Maksimova, Viktoria <viktoria.maksimova@intel.com>

--- lib/SPIRV/libSPIRV/spirv_internal.hpp
+++ lib/SPIRV/libSPIRV/spirv_internal.hpp
@@ -78,7 +78,6 @@ enum InternalCapability {
   ICapabilityHWThreadQueryINTEL = 6134,
   ICapFPArithmeticFenceINTEL = 6144,
   ICapGlobalVariableDecorationsINTEL = 6146,
-  ICapabilityNonConstantAddrspacePrintfINTEL = 6411,
   ICapabilityComplexFloatMulDivINTEL = 6414,
   ICapabilityTensorFloat32ConversionINTEL = 6425,
   ICapabilityMaskedGatherScatterINTEL = 6427
@@ -120,8 +119,6 @@ _SPIRV_OP(Op, JointMatrixWorkItemLengthINTEL)
 _SPIRV_OP(Capability, HWThreadQueryINTEL)
 _SPIRV_OP(BuiltIn, SubDeviceIDINTEL)
 _SPIRV_OP(BuiltIn, GlobalHWThreadIDINTEL)
-
-_SPIRV_OP(Capability, NonConstantAddrspacePrintfINTEL)
 
 _SPIRV_OP(Capability, ComplexFloatMulDivINTEL)
 _SPIRV_OP(Op, ComplexFMulINTEL)
