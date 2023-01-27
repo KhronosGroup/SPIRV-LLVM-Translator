db2b8553d8ebd64bf244661e6bded0c6e43c5457
Arvind Sudarsanam <arvind.sudarsanam@intel.com>
2023-01-26 20:18:46 -0800

    Address wrong change
    Signed-off-by: Arvind Sudarsanam <arvind.sudarsanam@intel.com>

--- lib/SPIRV/OCLToSPIRV.cpp
+++ lib/SPIRV/OCLToSPIRV.cpp
@@ -811,9 +811,8 @@ void OCLToSPIRVBase::visitCallGroupBuiltin(CallInst *CI,
                                   .Case("ballot_inclusive_scan", "add")
                                   .Case("ballot_exclusive_scan", "add")
                                   .Default(FuncName.take_back(
-                                      3)); // assumes op is three characters
-          if (!GroupOp.consume_front("_")) // when op is two characters
-            return true;                   // continue
+                                      3));    // assumes op is three characters
+          (void)(GroupOp.consume_front("_")); // when op is two characters
           assert(!GroupOp.empty() && "Invalid OpenCL group builtin function");
           char OpTyC = 0;
           auto OpTy = F->getReturnType();
