6eb75d861ff5ce11d5ce3a4a301d11f1adced2df
asudarsa <arvind.sudarsanam@intel.com>
2023-06-13 15:23:31 -0400

    [DebugInfo] Add debug info for bitfield members (#1907)
    Signed-off-by: Arvind Sudarsanam <arvind.sudarsanam@intel.com>

--- lib/SPIRV/SPIRVToLLVMDbgTran.cpp
+++ lib/SPIRV/SPIRVToLLVMDbgTran.cpp
@@ -668,7 +668,6 @@ SPIRVToLLVMDbgTran::transTypeMemberOpenCL(const SPIRVExtInst *DebugInst) {
   }
   if (SPIRVFlags & SPIRVDebug::FlagIsStaticMember)
     Flags |= DINode::FlagStaticMember;
-
   if (Flags & DINode::FlagStaticMember && Ops.size() > MinOperandCount) {
     SPIRVValue *ConstVal = BM->get<SPIRVValue>(Ops[ValueIdx]);
     assert(isConstantOpCode(ConstVal->getOpCode()) &&
@@ -716,6 +715,8 @@ SPIRVToLLVMDbgTran::transTypeMemberNonSemantic(const SPIRVExtInst *DebugInst,
   }
   if (SPIRVFlags & SPIRVDebug::FlagIsStaticMember)
     Flags |= DINode::FlagStaticMember;
+  if (SPIRVFlags & SPIRVDebug::FlagBitField)
+    Flags |= DINode::FlagBitField;
 
   if (Flags & DINode::FlagStaticMember && Ops.size() > MinOperandCount) {
     SPIRVValue *ConstVal = BM->get<SPIRVValue>(Ops[ValueIdx]);
