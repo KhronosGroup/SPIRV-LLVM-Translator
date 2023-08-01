7d3d381009686d3b51311db55ac8709b7937e5be
Viktoria Maximova <viktoria.maksimova@intel.com>
2023-08-02 00:37:14 +0200

    Initial implementation of SPV_KHR_cooperative_matrix extension (#2099)
    The intention is to replace existing SPV_INTEL_joint_matrix extension to the Khronos one in future.
    
    Spec:
    https://github.com/KhronosGroup/SPIRV-Registry/blob/main/extensions/KHR/SPV_KHR_cooperative_matrix.asciidoc

--- lib/SPIRV/libSPIRV/SPIRVOpCode.h
+++ lib/SPIRV/libSPIRV/SPIRVOpCode.h
@@ -224,7 +224,8 @@ inline bool isTypeOpCode(Op OpCode) {
   return (OpTypeVoid <= OC && OC <= OpTypePipe) || OC == OpTypePipeStorage ||
          isSubgroupAvcINTELTypeOpCode(OpCode) || OC == OpTypeVmeImageINTEL ||
          isVCOpCode(OpCode) || OC == internal::OpTypeTokenINTEL ||
-         OC == internal::OpTypeJointMatrixINTEL;
+         OC == internal::OpTypeJointMatrixINTEL ||
+         OC == OpTypeCooperativeMatrixKHR;
 }
 
 inline bool isSpecConstantOpCode(Op OpCode) {
