ad3a19e1caee3ed725f2e9322fff5bd6eba81596
Nick Sarnie <sarnex@users.noreply.github.com>
2023-10-13 12:51:25 +0000

    [Backport to 14] Backport spirv-preserve-auxdata changes (#2178)
    Backport the below changes to 14:
    
    f729c49
    89d658c
    9823690
    d498f48
    d24b9c6
    
    I made the following changes that are not in the original changes:
    
    Use llvm::Optional instead of std::optional
    Port tests to not use opaque pointers and related flags
    Fix patch fail due to missing unrelated function in this branch
    
    Signed-off-by: Sarnie, Nick <nick.sarnie@intel.com>

--- tools/llvm-spirv/llvm-spirv.cpp
+++ tools/llvm-spirv/llvm-spirv.cpp
@@ -183,6 +183,10 @@ static cl::opt<bool>
     SPIRVMemToReg("spirv-mem2reg", cl::init(false),
                   cl::desc("LLVM/SPIR-V translation enable mem2reg"));
 
+static cl::opt<bool> SPIRVPreserveAuxData(
+    "spirv-preserve-auxdata", cl::init(false),
+    cl::desc("Preserve all auxiliary data, such as function attributes and metadata"));
+
 static cl::opt<bool> SpecConstInfo(
     "spec-const-info",
     cl::desc("Display id of constants available for specializaion and their "
@@ -487,8 +491,11 @@ static int parseSPVExtOption(
   //  - during SPIR-V generation, assume that any known extension is disallowed.
   //  - during conversion to/from SPIR-V text representation, assume that any
   //    known extension is allowed.
+  llvm::Optional<bool> DefaultVal;
+  if (IsReverse)
+    DefaultVal = true;
   for (const auto &It : ExtensionNamesMap)
-    ExtensionsStatus[It.second] = IsReverse;
+    ExtensionsStatus[It.second] = DefaultVal;
 
   if (SPVExt.empty())
     return 0; // Nothing to do
@@ -688,6 +695,14 @@ int main(int Ac, char **Av) {
   if (IsReverse && !SpecConst.empty()) {
     if (parseSpecConstOpt(SpecConst, Opts))
       return -1;
+  }
+
+  if (SPIRVPreserveAuxData) {
+    Opts.setPreserveAuxData(
+        SPIRVPreserveAuxData);
+    if (!IsReverse)
+      Opts.setAllowedToUseExtension(
+          SPIRV::ExtensionID::SPV_KHR_non_semantic_info);
   }
 
   if (SPIRVAllowUnknownIntrinsics.getNumOccurrences() != 0) {
