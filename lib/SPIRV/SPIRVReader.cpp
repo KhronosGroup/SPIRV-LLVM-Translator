5011b375d1153e290d0364f0ca363d2a455b107e
bwlodarcz <bertrand.wlodarczyk@intel.com>
2023-10-13 12:33:06 +0200

    Add support for ISubBorrow SPIRV instruction (#2168)
    This commit implements bidirectional translation of the llvm.usub.with.overflow and the ISubBorrow intrinsic.
    Intrinsic llvm.usub.with.overflow returns struct which second element have a type of i1.
    The llvm type i1 is, in llvm-spirv, directly translated to BoolType.
    SPIRV specification requires that the composite which returns from ISubBorrow needs to have both elements of the same type.
    In result, current implementation is not compliant and should be considered temporary.

--- lib/SPIRV/SPIRVReader.cpp
+++ lib/SPIRV/SPIRVReader.cpp
@@ -2305,10 +2305,18 @@ Value *SPIRVToLLVM::transValueWithoutDecoration(SPIRVValue *BV, Function *F,
     return mapValue(BV,
                     transRelational(static_cast<SPIRVInstruction *>(BV), BB));
   case OpIAddCarry: {
-    IRBuilder Builder(BB);
+    IRBuilder<> Builder(BB);
     auto *BC = static_cast<SPIRVBinary *>(BV);
     return mapValue(BV, Builder.CreateBinaryIntrinsic(
                             Intrinsic::uadd_with_overflow,
+                            transValue(BC->getOperand(0), F, BB),
+                            transValue(BC->getOperand(1), F, BB)));
+  }
+  case OpISubBorrow: {
+    IRBuilder<> Builder(BB);
+    auto *BC = static_cast<SPIRVBinary *>(BV);
+    return mapValue(BV, Builder.CreateBinaryIntrinsic(
+                            Intrinsic::usub_with_overflow,
                             transValue(BC->getOperand(0), F, BB),
                             transValue(BC->getOperand(1), F, BB)));
   }
--- lib/SPIRV/SPIRVWriter.cpp
+++ lib/SPIRV/SPIRVWriter.cpp
@@ -2596,6 +2596,7 @@ bool LLVMToSPIRVBase::isKnownIntrinsic(Intrinsic::ID Id) {
   case Intrinsic::dbg_label:
   case Intrinsic::trap:
   case Intrinsic::uadd_with_overflow:
+  case Intrinsic::usub_with_overflow:
     return true;
   default:
     // Unknown intrinsics' declarations should always be translated
@@ -2968,6 +2969,11 @@ SPIRVValue *LLVMToSPIRVBase::transIntrinsicInst(IntrinsicInst *II,
   }
   case Intrinsic::uadd_with_overflow: {
     return BM->addBinaryInst(OpIAddCarry, transType(II->getType()),
+                             transValue(II->getArgOperand(0), BB),
+                             transValue(II->getArgOperand(1), BB), BB);
+  }
+  case Intrinsic::usub_with_overflow: {
+    return BM->addBinaryInst(OpISubBorrow, transType(II->getType()),
                              transValue(II->getArgOperand(0), BB),
                              transValue(II->getArgOperand(1), BB), BB);
   }
