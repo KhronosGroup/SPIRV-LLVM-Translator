f750da7f72d92434d438895081d2aef962bcbf42
Levytskyy, Vyacheslav <vyacheslav.levytskyy@intel.com>
2023-11-15 07:43:29 -0800

    fix _SPIRV_OP parameter: literal argument index

--- lib/SPIRV/libSPIRV/SPIRVInstruction.h
+++ lib/SPIRV/libSPIRV/SPIRVInstruction.h
@@ -3409,7 +3409,7 @@ protected:
   typedef SPIRVInstTemplate<SPIRVCooperativeMatrixPrefetchINTELInstBase,       \
                             internal::Op##x##INTEL, __VA_ARGS__>               \
       SPIRV##x##INTEL;
-_SPIRV_OP(CooperativeMatrixPrefetch, false, 8, true, 7)
+_SPIRV_OP(CooperativeMatrixPrefetch, false, 8, true, 5)
 #undef _SPIRV_OP
 
 class SPIRVCooperativeMatrixKHRInstBase : public SPIRVInstTemplateBase {
