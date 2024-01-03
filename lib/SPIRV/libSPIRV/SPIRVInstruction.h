b81d354c0688c19d436b1d29274c4b2edc1c1ee3
Wenju He <wenju.he@intel.com>
2024-01-03 21:28:00 +0800

    [SPV -> SPV-IR] Fix image builtin mangling of unsigned type (#2273)
    Return type of image read and Texel type of image write builtins may be
    unsigned. Before this PR, the builtin names in SPIR-V Friendly IR were
    always mangled with signed type.
    
    (cherry picked from commit e9b95fbc3f58c7d8084620fd58b8bd936c640d37)

--- lib/SPIRV/libSPIRV/SPIRVInstruction.h
+++ lib/SPIRV/libSPIRV/SPIRVInstruction.h
@@ -2808,9 +2808,6 @@ public:
 
 protected:
   void setOpWords(const std::vector<SPIRVWord> &OpsArg) override;
-
-private:
-  size_t getImageOperandsIndex() const;
 };
 
 #define _SPIRV_OP(x, ...)                                                      \
