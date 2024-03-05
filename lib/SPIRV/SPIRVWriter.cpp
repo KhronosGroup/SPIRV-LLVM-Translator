d0cc3122e0665367345524a8c0c482178c4f9a22
Wlodarczyk, Bertrand <bertrand.wlodarczyk@intel.com>
2024-03-05 15:03:38 +0100

    Format fix

--- lib/SPIRV/SPIRVWriter.cpp
+++ lib/SPIRV/SPIRVWriter.cpp
@@ -6388,16 +6388,16 @@ LLVMToSPIRVBase::transBuiltinToInstWithoutDecoration(Op OC, CallInst *CI,
     Function *F = CI->getCalledFunction();
     StructType *St = cast<StructType>(F->getParamStructRetType(0));
     SPIRVValue *V = BM->addBinaryInst(OpIAddCarry, transType(St),
-                      transValue(CI->getArgOperand(1), BB),
-                      transValue(CI->getArgOperand(2), BB), BB);
+                                      transValue(CI->getArgOperand(1), BB),
+                                      transValue(CI->getArgOperand(2), BB), BB);
     return BM->addStoreInst(transValue(CI->getArgOperand(0), BB), V, {}, BB);
   }
   case OpISubBorrow: {
     Function *F = CI->getCalledFunction();
     StructType *St = cast<StructType>(F->getParamStructRetType(0));
     SPIRVValue *V = BM->addBinaryInst(OpISubBorrow, transType(St),
-                             transValue(CI->getArgOperand(1), BB),
-                             transValue(CI->getArgOperand(2), BB), BB);
+                                      transValue(CI->getArgOperand(1), BB),
+                                      transValue(CI->getArgOperand(2), BB), BB);
     return BM->addStoreInst(transValue(CI->getArgOperand(0), BB), V, {}, BB);
   }
   case OpGroupNonUniformShuffleDown: {
