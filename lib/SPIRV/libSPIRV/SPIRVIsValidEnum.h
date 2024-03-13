16b2900747a3cc7993eeb39d4a8442b432a3fda0
Viktoria Maximova <viktoria.maksimova@intel.com>
2024-03-13 18:37:19 +0100

    [LLVM-18] Remove dependency on SPIRV-Headers for SPV_INTEL_maximum_registers (#2434)
    This fixes #2415

--- lib/SPIRV/libSPIRV/SPIRVIsValidEnum.h
+++ lib/SPIRV/libSPIRV/SPIRVIsValidEnum.h
@@ -73,9 +73,9 @@ inline bool isValid(spv::ExecutionModel V) {
   case ExecutionModelCallableKHR:
   case ExecutionModeRegisterMapInterfaceINTEL:
   case ExecutionModeStreamingInterfaceINTEL:
-  case ExecutionModeMaximumRegistersINTEL:
-  case ExecutionModeMaximumRegistersIdINTEL:
-  case ExecutionModeNamedMaximumRegistersINTEL:
+  case internal::ExecutionModeMaximumRegistersINTEL:
+  case internal::ExecutionModeMaximumRegistersIdINTEL:
+  case internal::ExecutionModeNamedMaximumRegistersINTEL:
     return true;
   default:
     return false;
