331248080be0dce13ce5c274aa0db5582832191f
Dmitry Sidorov <dmitry.sidorov@intel.com>
2024-04-25 16:37:10 +0200

    Add SPV_INTEL_bindless_images preview extension (#2535)
    Specification: https://github.com/intel/llvm/blob/sycl/sycl/doc/design/spirv-extensions/SPV_INTEL_bindless_images.asciidoc
    
    Co-author: He, Wenju wenju.he@intel.com

--- lib/SPIRV/SPIRVReader.cpp
+++ lib/SPIRV/SPIRVReader.cpp
@@ -3410,6 +3410,7 @@ Instruction *SPIRVToLLVM::transSPIRVBuiltinFromInst(SPIRVInstruction *BI,
   case internal::OpJointMatrixLoadINTEL:
   case OpCooperativeMatrixLoadKHR:
   case internal::OpCooperativeMatrixLoadCheckedINTEL:
+  case internal::OpConvertHandleToImageINTEL:
     AddRetTypePostfix = true;
     break;
   default: {
--- lib/SPIRV/SPIRVUtil.cpp
+++ lib/SPIRV/SPIRVUtil.cpp
@@ -1497,6 +1497,11 @@ std::string getSPIRVImageSampledTypeName(SPIRVType *Ty) {
       else
         return kSPIRVImageSampledTypeName::UInt;
     }
+    if (Ty->getIntegerBitWidth() == 64) {
+      if (static_cast<SPIRVTypeInt *>(Ty)->isSigned())
+        return kSPIRVImageSampledTypeName::Long;
+      return kSPIRVImageSampledTypeName::ULong;
+    }
     break;
   case OpTypeFloat:
     switch (Ty->getFloatBitWidth()) {
@@ -1528,6 +1533,9 @@ Type *getLLVMTypeForSPIRVImageSampledTypePostfix(StringRef Postfix,
   if (Postfix == kSPIRVImageSampledTypeName::Int ||
       Postfix == kSPIRVImageSampledTypeName::UInt)
     return Type::getInt32Ty(Ctx);
+  if (Postfix == kSPIRVImageSampledTypeName::Long ||
+      Postfix == kSPIRVImageSampledTypeName::ULong)
+    return Type::getInt64Ty(Ctx);
   llvm_unreachable("Invalid sampled type postfix");
   return nullptr;
 }
@@ -2219,7 +2227,7 @@ public:
 
   void init(StringRef UniqUnmangledName) override {
     UnmangledName = UniqUnmangledName.str();
-    switch (OC) {
+    switch (static_cast<unsigned>(OC)) {
     case OpConvertUToF:
     case OpUConvert:
     case OpSatConvertUToS:
@@ -2384,6 +2392,10 @@ public:
       }
       break;
     }
+    case internal::OpConvertHandleToImageINTEL:
+    case internal::OpConvertHandleToSamplerINTEL:
+      addUnsignedArg(0);
+      break;
     default:;
       // No special handling is needed
     }
