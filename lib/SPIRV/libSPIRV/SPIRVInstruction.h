ab1c3ebafbca25de42685b06c16def58bc17442c
DBDuncan <duncan.brawley@codeplay.com>
2024-05-21 11:30:16 +0800

    Add updated SPV_INTEL_bindless_images preview extension (#2559)
    The updated SPV_INTEL_bindless_images spec can be seen in this PR: intel/llvm#13753
    
    (cherry picked from commit e9a9a3fc1fd3e57449ba22d0065a373eaf4b1a53)

--- lib/SPIRV/libSPIRV/SPIRVInstruction.h
+++ lib/SPIRV/libSPIRV/SPIRVInstruction.h
@@ -3853,7 +3853,9 @@ protected:
     SPVErrLog.checkError(
         (ResTy->isTypeImage() && OC == internal::OpConvertHandleToImageINTEL) ||
             (ResTy->isTypeSampler() &&
-             OC == internal::OpConvertHandleToSamplerINTEL),
+             OC == internal::OpConvertHandleToSamplerINTEL) ||
+            (ResTy->isTypeSampledImage() &&
+             OC == internal::OpConvertHandleToSampledImageINTEL),
         SPIRVEC_InvalidInstruction,
         InstName + "\nIncorrect return type of the instruction must be "
                    "image/sampler\n");
@@ -3863,6 +3865,7 @@ protected:
   typedef SPIRVBindlessImagesInstBase<internal::Op##x> SPIRV##x;
 _SPIRV_OP(ConvertHandleToImageINTEL)
 _SPIRV_OP(ConvertHandleToSamplerINTEL)
+_SPIRV_OP(ConvertHandleToSampledImageINTEL)
 #undef _SPIRV_OP
 
 } // namespace SPIRV
