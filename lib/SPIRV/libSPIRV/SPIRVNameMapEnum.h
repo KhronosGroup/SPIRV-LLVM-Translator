e73494f8f4e693a9ef312c542216f3054aedf565
Mateusz Chudyk <mateusz.chudyk@intel.com>
2024-05-27 00:44:54 +0200

    [Backport to 17] Support both legacy and Khronos version of global variable extensions (#2228) (#2573)
    This is done to provide smooth drivers transition to official
    extensions.
    Also updated tests for new extensions to use new tokens after
    https://github.com/KhronosGroup/SPIRV-Headers/commit/cca08c63cefa129d082abca0302adcb81610b465
    
    Co-authored-by: Viktoria Maximova <viktoria.maksimova@intel.com>

--- lib/SPIRV/libSPIRV/SPIRVNameMapEnum.h
+++ lib/SPIRV/libSPIRV/SPIRVNameMapEnum.h
@@ -207,6 +207,9 @@ template <> inline void SPIRVMap<Decoration, std::string>::init() {
   // From spirv_internal.hpp
   add(internal::DecorationCallableFunctionINTEL, "CallableFunctionINTEL");
   add(internal::DecorationRuntimeAlignedINTEL, "RuntimeAlignedINTEL");
+  add(internal::DecorationHostAccessINTEL, "HostAccessINTEL");
+  add(internal::DecorationInitModeINTEL, "InitModeINTEL");
+  add(internal::DecorationImplementInCSRINTEL, "ImplementInCSRINTEL");
   add(internal::DecorationArgumentAttributeINTEL, "ArgumentAttributeINTEL");
   add(internal::DecorationCacheControlLoadINTEL, "CacheControlLoadINTEL");
   add(internal::DecorationCacheControlStoreINTEL, "CacheControlStoreINTEL");
@@ -637,6 +640,8 @@ template <> inline void SPIRVMap<Capability, std::string>::init() {
   add(internal::CapabilityBfloat16ConversionINTEL, "Bfloat16ConversionINTEL");
   add(internal::CapabilityJointMatrixINTEL, "JointMatrixINTEL");
   add(internal::CapabilityHWThreadQueryINTEL, "HWThreadQueryINTEL");
+  add(internal::CapabilityGlobalVariableDecorationsINTEL,
+      "GlobalVariableDecorationsINTEL");
   add(internal::CapabilityComplexFloatMulDivINTEL, "ComplexFloatMulDivINTEL");
   add(internal::CapabilityMaskedGatherScatterINTEL, "MaskedGatherScatterINTEL");
   add(internal::CapabilityTensorFloat32RoundingINTEL,
