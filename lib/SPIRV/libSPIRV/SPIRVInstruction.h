47daab3843f3f28fd2a2e3410ee9740fddc296b8
Viktoria Maximova <viktoria.maksimova@intel.com>
2024-07-02 12:58:27 +0200

    Removed OpLessOrGreater (#2527)

--- lib/SPIRV/libSPIRV/SPIRVInstruction.h
+++ lib/SPIRV/libSPIRV/SPIRVInstruction.h
@@ -981,6 +981,10 @@ protected:
       ResTy = Type;
     }
     assert(isCmpOpCode(OpCode) && "Invalid op code for cmp inst");
+    if (OpCode == OpLessOrGreater)
+      assert(this->getModule()->getSPIRVVersion() <=
+                 static_cast<SPIRVWord>(VersionNumber::SPIRV_1_5) &&
+             "OpLessOrGreater is removed starting from SPIR-V 1.6");
     assert((ResTy->isTypeBool() || ResTy->isTypeInt()) &&
            "Invalid type for compare instruction");
     assert(Op1Ty == Op2Ty && "Inconsistent types");
