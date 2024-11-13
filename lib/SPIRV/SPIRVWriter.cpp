23b14ed3d88418e0f0c1025aff699e3b132f7dbe
Jinsong Ji <jinsong.ji@intel.com>
2024-11-13 05:11:44 -0500

    Remove unused var (#2854)

--- lib/SPIRV/SPIRVWriter.cpp
+++ lib/SPIRV/SPIRVWriter.cpp
@@ -3121,7 +3121,6 @@ bool LLVMToSPIRVBase::transDecoration(Value *V, SPIRVValue *BV) {
         if (RetAttrs.hasAttribute(Attribute::NoFPClass)) {
           FPClassTest RetTest =
               RetAttrs.getAttribute(Attribute::NoFPClass).getNoFPClass();
-          AttributeSet RetAttrs = FAttrs.getRetAttrs();
           // Only Nan and Inf tests are representable in SPIR-V now.
           bool ToAddNoNan = RetTest & fcNan;
           bool ToAddNoInf = RetTest & fcInf;
