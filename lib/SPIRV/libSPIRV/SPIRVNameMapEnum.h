20ebfeda57bbd8a3b0faa36f2e6ff8fd486f24dd
Yury Plyakhin <yury.plyakhin@intel.com>
2025-01-28 11:10:05 -0800

    [Backport to 18] Implement SPV_INTEL_subgroup_matrix_multiply_accumulate (#2822)
    Spec: https://github.khronos.org/SPIRV-Registry/extensions/INTEL/SPV_INTEL_subgroup_matrix_multiply_accumulate.html

--- lib/SPIRV/libSPIRV/SPIRVNameMapEnum.h
+++ lib/SPIRV/libSPIRV/SPIRVNameMapEnum.h
@@ -644,6 +644,8 @@ template <> inline void SPIRVMap<Capability, std::string>::init() {
   add(CapabilitySubgroup2DBlockIOINTEL, "Subgroup2DBlockIOINTEL");
   add(CapabilitySubgroup2DBlockTransformINTEL, "Subgroup2DBlockTransformINTEL");
   add(CapabilitySubgroup2DBlockTransposeINTEL, "Subgroup2DBlockTransposeINTEL");
+  add(CapabilitySubgroupMatrixMultiplyAccumulateINTEL,
+      "SubgroupMatrixMultiplyAccumulateINTEL");
   // From spirv_internal.hpp
   add(internal::CapabilityFastCompositeINTEL, "FastCompositeINTEL");
   add(internal::CapabilityTokenTypeINTEL, "TokenTypeINTEL");
