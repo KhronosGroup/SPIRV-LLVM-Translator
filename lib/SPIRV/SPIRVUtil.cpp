e0f4b4fce11a444aa949b002328773b4ceeb7f56
Kanclerz, Piotr <piotr.kanclerz@intel.com>
2025-02-13 10:34:23 +0100

    [Backport to 17]Add updated SPV_INTEL_bindless_images preview extension (#2559)
    The updated SPV_INTEL_bindless_images spec can be seen in this PR: intel/llvm#13753

--- lib/SPIRV/SPIRVUtil.cpp
+++ lib/SPIRV/SPIRVUtil.cpp
@@ -2525,6 +2525,7 @@ public:
     }
     case internal::OpConvertHandleToImageINTEL:
     case internal::OpConvertHandleToSamplerINTEL:
+    case internal::OpConvertHandleToSampledImageINTEL:
       addUnsignedArg(0);
       break;
     default:;
--- lib/SPIRV/libSPIRV/spirv_internal.hpp
+++ lib/SPIRV/libSPIRV/spirv_internal.hpp
@@ -83,6 +83,7 @@ enum InternalOp {
   IOpCooperativeMatrixPrefetchINTEL = 6449,
   IOpConvertHandleToImageINTEL = 6529,
   IOpConvertHandleToSamplerINTEL = 6530,
+  IOpConvertHandleToSampledImageINTEL = 6531,
   IOpPrev = OpMax - 2,
   IOpForward
 };
@@ -221,6 +222,7 @@ _SPIRV_OP(Capability, CacheControlsINTEL)
 _SPIRV_OP(Capability, BindlessImagesINTEL)
 _SPIRV_OP(Op, ConvertHandleToImageINTEL)
 _SPIRV_OP(Op, ConvertHandleToSamplerINTEL)
+_SPIRV_OP(Op, ConvertHandleToSampledImageINTEL)
 #undef _SPIRV_OP
 
 constexpr SourceLanguage SourceLanguagePython =
