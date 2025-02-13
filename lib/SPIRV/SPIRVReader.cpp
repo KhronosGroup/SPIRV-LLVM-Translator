07dd40a48239a84f8de586c7f57521bc21b97992
Kanclerz, Piotr <piotr.kanclerz@intel.com>
2025-02-13 10:36:01 +0100

    [Backport to 17][SPV_INTEL_bindless_images] Add ret type to convert-SampledImage builtin name (#2576)
    Similar as ConvertHandleToImageINTEL, ConvertHandleToSampledImageINTEL
    builtin may have the same argument type but different return types.
    So we need to add ret type as name suffix when translating to LLVM IR.

--- lib/SPIRV/SPIRVReader.cpp
+++ lib/SPIRV/SPIRVReader.cpp
@@ -3584,6 +3584,7 @@ Instruction *SPIRVToLLVM::transSPIRVBuiltinFromInst(SPIRVInstruction *BI,
   case OpCooperativeMatrixLoadKHR:
   case internal::OpCooperativeMatrixLoadCheckedINTEL:
   case internal::OpConvertHandleToImageINTEL:
+  case internal::OpConvertHandleToSampledImageINTEL:
     AddRetTypePostfix = true;
     break;
   default: {
