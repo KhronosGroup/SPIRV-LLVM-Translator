ccd8135cb0ccae62ca768c8a44154d4d6e8e4c38
Wenju He <wenju.he@intel.com>
2025-03-24 13:37:01 +0800

    change NumElements/Stride of OpGroupAsyncCopy to unsigned

--- lib/SPIRV/SPIRVUtil.cpp
+++ lib/SPIRV/SPIRVUtil.cpp
@@ -2410,6 +2410,10 @@ public:
       addUnsignedArg(0);
       addUnsignedArg(3);
       break;
+    case OpGroupAsyncCopy:
+      addUnsignedArg(3);
+      addUnsignedArg(4);
+      break;
     case OpGroupUMax:
     case OpGroupUMin:
     case OpGroupBroadcast:
