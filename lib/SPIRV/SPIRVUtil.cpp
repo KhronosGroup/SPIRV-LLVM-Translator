2aa4ac20f06b4cee1e788d43deeaa584a1275d8c
Wenju He <wenju.he@intel.com>
2025-03-25 14:46:54 +0000

    [SPIRV-to-LLVM] Set LocalId parameter of GroupBroadcast to unsigned in SPV-IR (#3084)
    LocalId should not be negative. Change its type from signed to unsigned
    in SPV-IR aligns with OpenCL work_group_broadcast built-in and use of
    GroupBroadcast built-in in SYCL header.
    
    Also change NumElements/Stride parameters of OpGroupAsyncCopy to
    unsigned.

--- lib/SPIRV/SPIRVUtil.cpp
+++ lib/SPIRV/SPIRVUtil.cpp
@@ -2410,8 +2410,13 @@ public:
       addUnsignedArg(0);
       addUnsignedArg(3);
       break;
+    case OpGroupAsyncCopy:
+      addUnsignedArg(3);
+      addUnsignedArg(4);
+      break;
     case OpGroupUMax:
     case OpGroupUMin:
+    case OpGroupBroadcast:
     case OpGroupNonUniformBroadcast:
     case OpGroupNonUniformBallotBitCount:
     case OpGroupNonUniformShuffle:
