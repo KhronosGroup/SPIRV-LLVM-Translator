e8679909a40d7d60adfb7d802ae05551f96f942e
Wenju He <wenju.he@intel.com>
2025-04-08 09:20:51 +0000

    [LLVM->SPV-IR] Set an arg of __spirv_ocl_nan/shuffle/shuffle2 to unsigned (#3106)
    OpenCL built-in nan's argument type and shuffle/shuffle2's mask argument
    type is unsigned. DPC++ header also sets unsigned type for them. So this
    PR changes them to unsigned in SPV-IR.

--- lib/SPIRV/SPIRVUtil.cpp
+++ lib/SPIRV/SPIRVUtil.cpp
@@ -2614,6 +2614,15 @@ public:
     case OpenCLLIB::S_Upsample:
       addUnsignedArg(1);
       break;
+    case OpenCLLIB::Nan:
+      addUnsignedArg(0);
+      break;
+    case OpenCLLIB::Shuffle:
+      addUnsignedArg(1);
+      break;
+    case OpenCLLIB::Shuffle2:
+      addUnsignedArg(2);
+      break;
     default:;
       // No special handling is needed
     }
