d250449f810f4ab1b4389bce9c95889b95b66a04
Dmitry Sidorov <dmitry.sidorov@intel.com>
2025-08-07 14:00:13 +0200

    [Backport to 18] Add SPV_INTEL_int4 extension (#3178) (#3296)
    Adds support for native 4-bit type.
    
    Spec:
    
    
    https://github.com/KhronosGroup/SPIRV-Registry/blob/main/extensions/INTEL/SPV_INTEL_int4.asciidoc
    
    Signed-off-by: Sidorov, Dmitry <dmitry.sidorov@intel.com>
    Co-authored-by: Viktoria Maximova <viktoria.maksimova@intel.com>

--- lib/SPIRV/libSPIRV/SPIRVNameMapEnum.h
+++ lib/SPIRV/libSPIRV/SPIRVNameMapEnum.h
@@ -677,6 +677,8 @@ template <> inline void SPIRVMap<Capability, std::string>::init() {
   add(internal::CapabilityCooperativeMatrixCheckedInstructionsINTEL,
       "CooperativeMatrixCheckedInstructionsINTEL");
   add(internal::CapabilityBindlessImagesINTEL, "BindlessImagesINTEL");
+  add(CapabilityInt4TypeINTEL, "Int4TypeINTEL");
+  add(CapabilityInt4CooperativeMatrixINTEL, "Int4CooperativeMatrixINTEL");
 }
 SPIRV_DEF_NAMEMAP(Capability, SPIRVCapabilityNameMap)
 
