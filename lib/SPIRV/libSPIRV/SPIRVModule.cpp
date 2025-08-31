b7b12ab007b2a4a13b8b87b4c8bf8bb1ed5e1746
Dmitry Sidorov <dmitry.sidorov@intel.com>
2025-08-31 19:29:30 +0200

    [Backport to 14] Implement SPV_INTEL_bfloat16_arithmetic (#3290) (#3320) (#3340)
    The extension relaxes rules for bf16 type allowing to use it in some
    arithmetic operations.
    
    Spec is available here:
    https://github.com/intel/llvm/pull/18352
    
    Co-authered by: Michael Aziz <michael.aziz@intel.com>
    
    ---------
    
    Signed-off-by: Sidorov, Dmitry <dmitry.sidorov@intel.com>

--- lib/SPIRV/libSPIRV/SPIRVModule.cpp
+++ lib/SPIRV/libSPIRV/SPIRVModule.cpp
@@ -1543,6 +1543,8 @@ SPIRVInstruction *SPIRVModuleImpl::addBinaryInst(Op TheOpCode, SPIRVType *Type,
                                                  SPIRVValue *Op1,
                                                  SPIRVValue *Op2,
                                                  SPIRVBasicBlock *BB) {
+  if (Type->isTypeFloat(16, FPEncodingBFloat16KHR) && TheOpCode != OpDot)
+    addCapability(internal::CapabilityBFloat16ArithmeticINTEL);
   return addInstruction(SPIRVInstTemplateBase::create(
                             TheOpCode, Type, getId(),
                             getVec(Op1->getId(), Op2->getId()), BB, this),
@@ -1566,6 +1568,8 @@ SPIRVInstruction *SPIRVModuleImpl::addUnaryInst(Op TheOpCode,
                                                 SPIRVType *TheType,
                                                 SPIRVValue *Op,
                                                 SPIRVBasicBlock *BB) {
+  if (TheType->isTypeFloat(16, FPEncodingBFloat16KHR) && TheOpCode != OpDot)
+    addCapability(internal::CapabilityBFloat16ArithmeticINTEL);
   return addInstruction(
       SPIRVInstTemplateBase::create(TheOpCode, TheType, getId(),
                                     getVec(Op->getId()), BB, this),
