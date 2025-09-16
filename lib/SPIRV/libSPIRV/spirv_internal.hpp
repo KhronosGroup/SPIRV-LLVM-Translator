2f472a154fae5bab60df43c0e778341a6916fdc2
Dmitry Sidorov <dmitry.sidorov@intel.com>
2025-09-16 11:22:24 +0200

    [Backport to 19] Support for SPV_INTEL_shader_atomic_bfloat16 extension (#3343) (#3347)
    Spec is available here:
    https://github.com/intel/llvm/pull/20009
    
    Author:
    "Ratajewski, Andrzej" <andrzej.ratajewski@intel.com>
    
    ---------
    
    Signed-off-by: Sidorov, Dmitry <dmitry.sidorov@intel.com>

--- lib/SPIRV/libSPIRV/spirv_internal.hpp
+++ lib/SPIRV/libSPIRV/spirv_internal.hpp
@@ -111,6 +111,8 @@ enum InternalCapability {
   ICapabilityTaskSequenceINTEL = 6162,
   ICapabilityCooperativeMatrixCheckedInstructionsINTEL = 6192,
   ICapabilityBFloat16ArithmeticINTEL = 6226,
+  ICapabilityAtomicBFloat16AddINTEL = 6255,
+  ICapabilityAtomicBFloat16MinMaxINTEL = 6256,
   ICapabilityCooperativeMatrixPrefetchINTEL = 6411,
   ICapabilityComplexFloatMulDivINTEL = 6414,
   ICapabilityTensorFloat32RoundingINTEL = 6425,
@@ -211,6 +213,9 @@ _SPIRV_OP(Capability, BindlessImagesINTEL)
 _SPIRV_OP(Op, ConvertHandleToImageINTEL)
 _SPIRV_OP(Op, ConvertHandleToSamplerINTEL)
 _SPIRV_OP(Op, ConvertHandleToSampledImageINTEL)
+
+_SPIRV_OP(Capability, AtomicBFloat16AddINTEL)
+_SPIRV_OP(Capability, AtomicBFloat16MinMaxINTEL)
 #undef _SPIRV_OP
 
 constexpr SourceLanguage SourceLanguagePython =
