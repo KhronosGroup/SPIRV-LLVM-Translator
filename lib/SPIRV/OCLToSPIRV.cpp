9efc940d994dec4a785d06a36556f0330e26b66e
Ben Ashbaugh <ben.ashbaugh@intel.com>
2025-09-22 05:15:46 -0700

    changes for llvm 14

--- lib/SPIRV/OCLToSPIRV.cpp
+++ lib/SPIRV/OCLToSPIRV.cpp
@@ -320,7 +320,7 @@ void OCLToSPIRVBase::visitCallInst(CallInst &CI) {
     visitCallDot(&CI, MangledName, DemangledName);
     return;
   }
-  if (DemangledName.starts_with(kOCLBuiltinName::ClockReadPrefix)) {
+  if (DemangledName.startswith(kOCLBuiltinName::ClockReadPrefix)) {
     visitCallClockRead(&CI, MangledName, DemangledName);
     return;
   }
@@ -1431,17 +1431,19 @@ void OCLToSPIRVBase::visitCallClockRead(CallInst *CI, StringRef MangledName,
                                         StringRef DemangledName) {
   // The builtin returns i64 or <2 x i32>, but both variants are mapped to the
   // same instruction; hence include the return type.
-  std::string OpName = getSPIRVFuncName(OpReadClockKHR, CI->getType());
+  const std::string OpName = getSPIRVFuncName(OpReadClockKHR, CI->getType());
 
   // Scope is part of the OpenCL builtin name.
-  Scope ScopeArg = StringSwitch<Scope>(DemangledName)
-                       .EndsWith("device", ScopeDevice)
-                       .EndsWith("work_group", ScopeWorkgroup)
-                       .EndsWith("sub_group", ScopeSubgroup)
-                       .Default(ScopeMax);
+  const Scope ScopeArg = StringSwitch<Scope>(DemangledName)
+                             .EndsWith("device", ScopeDevice)
+                             .EndsWith("work_group", ScopeWorkgroup)
+                             .EndsWith("sub_group", ScopeSubgroup)
+                             .Default(ScopeMax);
 
-  auto Mutator = mutateCallInst(CI, OpName);
-  Mutator.appendArg(getInt32(M, ScopeArg));
+  mutateCallInstSPIRV(M, CI, [=](CallInst *, std::vector<Value *> &Args) {
+    Args.push_back(getInt32(M, ScopeArg));
+    return OpName;
+  });
 }
 
 void OCLToSPIRVBase::visitCallScalToVec(CallInst *CI, StringRef MangledName,
