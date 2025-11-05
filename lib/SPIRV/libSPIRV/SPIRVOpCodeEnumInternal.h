379f0a1ebf5b7ee3d37eefded31fb4eaae687bfc
Vadim Semenov <vadim.semenov@intel.com>
2025-11-05 02:57:40 -0800

    [Backport to llvm_release_160] Implement SPV_INTEL_sigmoid extension (#3429)
    Spec link: https://github.com/intel/llvm/pull/20504

--- lib/SPIRV/libSPIRV/SPIRVOpCodeEnumInternal.h
+++ lib/SPIRV/libSPIRV/SPIRVOpCodeEnumInternal.h
@@ -38,3 +38,4 @@ _SPIRV_OP_INTERNAL(PredicatedLoadINTEL,
                    internal::OpPredicatedLoadINTEL)
 _SPIRV_OP_INTERNAL(PredicatedStoreINTEL,
                    internal::OpPredicatedStoreINTEL)
+_SPIRV_OP_INTERNAL(FSigmoidINTEL, internal::FSigmoidINTEL)
