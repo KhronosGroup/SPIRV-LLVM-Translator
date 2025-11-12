896857383b8377efcbd01d69bb9b63e18756ad0a
Dmitry Sidorov <dmitry.sidorov@intel.com>
2025-11-12 13:59:17 +0100

    [Backport to 15] Implement SPV_INTEL_float4 and SPV_INTEL_fp_conversions extensions (#3419)
    As well as their appropriate conversions via __builtin_spirv mechanism.
    
    Specification: https://github.com/intel/llvm/pull/20467
    
    Signed-off-by: Dmitry Sidorov <dmitry.sidorov@intel.com>

--- lib/SPIRV/libSPIRV/SPIRVInstruction.h
+++ lib/SPIRV/libSPIRV/SPIRVInstruction.h
@@ -48,6 +48,8 @@
 #include "SPIRVStream.h"
 #include "SPIRVValue.h"
 
+#include "llvm/ADT/Optional.h"
+
 #include <cassert>
 #include <functional>
 #include <iostream>
@@ -4145,7 +4147,7 @@ _SPIRV_OP(PredicatedStore, false, 4, true)
 #undef _SPIRV_OP
 
 template <Op OC> class SPIRVFSigmoidINTELInstBase : public SPIRVUnaryInst<OC> {
-protected:
+public:
   SPIRVCapVec getRequiredCapability() const override {
     return getVec(internal::CapabilitySigmoidINTEL);
   }
@@ -4201,6 +4203,27 @@ protected:
 #define _SPIRV_OP(x, ...)                                                      \
   typedef SPIRVFSigmoidINTELInstBase<internal::Op##x> SPIRV##x;
 _SPIRV_OP(FSigmoidINTEL)
+#undef _SPIRV_OP
+
+class SPIRVFPConversionINTELInstBase : public SPIRVInstTemplateBase {
+public:
+  SPIRVCapVec getRequiredCapability() const override {
+    return getVec(internal::CapabilityFloatConversionsINTEL);
+  }
+
+  llvm::Optional<ExtensionID> getRequiredExtension() const override {
+    return ExtensionID::SPV_INTEL_fp_conversions;
+  }
+};
+#define _SPIRV_OP(x, ...)                                                      \
+  typedef SPIRVInstTemplate<SPIRVFPConversionINTELInstBase,                    \
+                            internal::Op##x##INTEL, __VA_ARGS__>               \
+      SPIRV##x##INTEL;
+_SPIRV_OP(ClampConvertFToF, true, 4, false)
+_SPIRV_OP(ClampConvertFToS, true, 4, false)
+_SPIRV_OP(StochasticRoundFToF, true, 5, true)
+_SPIRV_OP(ClampStochasticRoundFToF, true, 5, true)
+_SPIRV_OP(ClampStochasticRoundFToS, true, 5, true)
 #undef _SPIRV_OP
 } // namespace SPIRV
 #endif // SPIRV_LIBSPIRV_SPIRVINSTRUCTION_H
