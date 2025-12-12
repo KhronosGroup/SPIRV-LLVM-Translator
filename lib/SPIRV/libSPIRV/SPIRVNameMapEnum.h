199d2e027d650a5048c5322c587da41867a1221b
Viktoria Maximova <viktoria.maksimova@intel.com>
2025-12-12 13:49:57 +0100

    Implement SPV_KHR_fma extension (#3467)
    The extension adds support for the `OpFmaKHR` instruction, which
    provides a native SPIR-V instruction for fused multiply-add operations
    as an alternative to using OpenCL.std::Fma extended instruction.
    
    Translate both LLVM fma intrinsics as well as OCL builtins to `OpFmaKHR`
    if the extension is available.
    
    Specification:
    https://github.khronos.org/SPIRV-Registry/extensions/KHR/SPV_KHR_fma.html

--- lib/SPIRV/libSPIRV/SPIRVNameMapEnum.h
+++ lib/SPIRV/libSPIRV/SPIRVNameMapEnum.h
@@ -657,6 +657,7 @@ template <> inline void SPIRVMap<Capability, std::string>::init() {
   add(CapabilitySubgroupMatrixMultiplyAccumulateINTEL,
       "SubgroupMatrixMultiplyAccumulateINTEL");
   add(CapabilityTernaryBitwiseFunctionINTEL, "TernaryBitwiseFunctionINTEL");
+  add(CapabilityFMAKHR, "FMAKHR");
   // From spirv_internal.hpp
   add(internal::CapabilityTokenTypeINTEL, "TokenTypeINTEL");
   add(internal::CapabilityHWThreadQueryINTEL, "HWThreadQueryINTEL");
