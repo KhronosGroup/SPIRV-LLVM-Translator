a837e326b53f2627e6598e119c463198fe839ccb
Wenju He <wenju.he@intel.com>
2026-01-07 05:32:22 +0800

    [Backport to 21][LLVM->SPV-IR] Add const to GroupAsyncCopy/GenericPtrMemSemantics/vload* source pointer (#3480)
    Movtivation is similar to 96f5ade2:
    * Align with OpenCL builtin.
    * Align with their declarations in
    https://github.com/intel/llvm/blob/sycl/clang/lib/Sema/SPIRVBuiltins.td.
    * Targets consuming SPV-IR don't need to implement both const and
    non-const builtin variants; that duplication only bloats the builtin
    library without any benefit.

--- lib/SPIRV/SPIRVUtil.cpp
+++ lib/SPIRV/SPIRVUtil.cpp
@@ -2448,6 +2448,7 @@ public:
       addUnsignedArg(3);
       break;
     case OpGroupAsyncCopy:
+      setArgAttr(2, SPIR::ATTR_CONST);
       addUnsignedArg(3);
       addUnsignedArg(4);
       break;
@@ -2591,6 +2592,9 @@ public:
     case internal::OpConvertHandleToSampledImageINTEL:
       addUnsignedArg(0);
       break;
+    case OpGenericPtrMemSemantics:
+      setArgAttr(0, SPIR::ATTR_CONST);
+      break;
     default:;
       // No special handling is needed
     }
@@ -2667,6 +2671,7 @@ public:
     case OpenCLLIB::Vload_halfn:
     case OpenCLLIB::Vloada_halfn:
       addUnsignedArg(0);
+      setArgAttr(1, SPIR::ATTR_CONST);
       break;
     case OpenCLLIB::Vstoren:
     case OpenCLLIB::Vstore_half:
