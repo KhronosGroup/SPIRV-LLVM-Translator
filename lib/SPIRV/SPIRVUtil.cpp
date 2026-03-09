a1f767ca3eba247bc5399161a709c967c4e6abf4
Viktoria Maximova <viktoria.maksimova@intel.com>
2026-03-09 11:07:03 +0100

    [Backport to 20] Update OCL nan builtin translation to work with bfloat type (#3558)
    To avoid name clash between builtins with `half` and `bfloat` return types, introduce return type postfix for
    `bfloat nan` builtin in reverse translation to SPIR-V Friendly IR.

--- lib/SPIRV/SPIRVUtil.cpp
+++ lib/SPIRV/SPIRVUtil.cpp
@@ -2601,7 +2601,7 @@ class OpenCLStdToSPIRVFriendlyIRMangleInfo : public BuiltinFuncMangleInfo {
 public:
   OpenCLStdToSPIRVFriendlyIRMangleInfo(OCLExtOpKind ExtOpId,
                                        ArrayRef<Type *> ArgTys, Type *RetTy)
-      : ExtOpId(ExtOpId), ArgTys(ArgTys) {
+      : ExtOpId(ExtOpId), ArgTys(ArgTys), RetTy(RetTy) {
 
     std::string Postfix = "";
     if (needRetTypePostfix())
@@ -2617,6 +2617,11 @@ public:
     case OpenCLLIB::Vloada_halfn:
     case OpenCLLIB::Vloadn:
       return true;
+    case OpenCLLIB::Nan:
+      // Only add return type mangling for bfloat16 to disambiguate from half
+      // (both are represented as i16 in LLVM). Float and half use traditional
+      // naming for backward compatibility.
+      return RetTy->getScalarType()->isBFloatTy();
     default:
       return false;
     }
@@ -2669,6 +2674,7 @@ public:
 private:
   OCLExtOpKind ExtOpId;
   ArrayRef<Type *> ArgTys;
+  Type *RetTy;
 };
 } // namespace
 
