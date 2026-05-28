7dc0b3fb3c0ac44c923f550ae87fc8e1c5eff3d6
Marcos Maronas <mmaronas@amd.com>
2026-05-28 08:57:58 -0500

    Translate llvm.exp10.* intrinsics

--- lib/SPIRV/SPIRVUtil.cpp
+++ lib/SPIRV/SPIRVUtil.cpp
@@ -1955,6 +1955,7 @@ bool checkTypeForSPIRVExtendedInstLowering(IntrinsicInst *II, SPIRVModule *BM) {
   case Intrinsic::cosh:
   case Intrinsic::exp:
   case Intrinsic::exp2:
+  case Intrinsic::exp10:
   case Intrinsic::fabs:
   case Intrinsic::floor:
   case Intrinsic::fma:
