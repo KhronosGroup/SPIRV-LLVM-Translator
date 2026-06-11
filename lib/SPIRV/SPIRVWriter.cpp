c2083371a1976ed72592ce56b1313bea2a040e28
Arseniy Obolenskiy <arseniy.obolenskiy@amd.com>
2026-06-11 14:03:09 +0200

    Address review comment

--- lib/SPIRV/SPIRVWriter.cpp
+++ lib/SPIRV/SPIRVWriter.cpp
@@ -2146,7 +2146,7 @@ LLVMToSPIRVBase::transValueWithoutDecoration(Value *V, SPIRVBasicBlock *BB,
           Elements.assign(ArrTy->getNumElements(), EltUndef);
         } else {
           for (Type *E : cast<StructType>(AggTy)->elements())
-            Elements.push_back(transValue(UndefValue::get(E), nullptr));
+            Elements.emplace_back(transValue(UndefValue::get(E), nullptr));
         }
         BVarInit = BM->addCompositeConstant(transType(AggTy), Elements);
         ValueMap[Init] = BVarInit;
@@ -4517,7 +4517,8 @@ SPIRVValue *LLVMToSPIRVBase::transIntrinsicInst(IntrinsicInst *II,
     SPIRVType *IntegralTy = transType(II->getType()->getStructElementType(1));
     // IntegralTy is the type of the result. We want to create a pointer to this
     // that we can pass to OpenCLLIB::modf to store the integral part.
-    SPIRVType *GenericPtrTy = BM->addPointerType(StorageClassFunction, IntegralTy);
+    SPIRVType *GenericPtrTy =
+        BM->addPointerType(StorageClassFunction, IntegralTy);
     auto *IntegralPtrTy = dyn_cast<SPIRVTypePointer>(GenericPtrTy);
     // We need to use the entry BB of the function calling llvm.modf.*, instead
     // of the current BB. For that, we'll find current BB's parent and get its
