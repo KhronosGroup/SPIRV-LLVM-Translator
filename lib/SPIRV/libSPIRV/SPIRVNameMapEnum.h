688effdb7e33c39bd01e097ef5fcbbb07d3332a0
Ye, Qi <qi.ye@intel.com>
2026-06-26 07:49:51 -0700

    [Backport to 17] Support SPV_INTEL_rounded_div_sqrt extension (#3770)
    Allow `fdiv` or `sqrt` instructions to be decorated with `FPRoundingMode`.
    
    Spec:
    https://github.khronos.org/SPIRV-Registry/extensions/INTEL/SPV_INTEL_rounded_divide_sqrt.html

--- lib/SPIRV/libSPIRV/SPIRVNameMapEnum.h
+++ lib/SPIRV/libSPIRV/SPIRVNameMapEnum.h
@@ -702,6 +702,7 @@ template <> inline void SPIRVMap<Capability, std::string>::init() {
       "FloatConversionsFtoFINTEL");
   add(internal::CapabilityFloatConversionsFtoSINTEL,
       "FloatConversionsFtoSINTEL");
+  add(CapabilityRoundedDivideSqrtINTEL, "RoundedDivideSqrtINTEL");
 }
 SPIRV_DEF_NAMEMAP(Capability, SPIRVCapabilityNameMap)
 
