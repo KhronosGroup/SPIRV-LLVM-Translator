57ff3df17bd878722366f11d78b90e5dc57c2719
Qi Ye <qi.ye@intel.com>
2026-07-17 10:25:24 -0400

    [Backport to 17] Add missing coverage for SPV_EXT_long_vector (#3869)
    This is a quick bug fix PR, as a follow-up of
    https://github.com/KhronosGroup/SPIRV-LLVM-Translator/pull/3777, which
    didn't cover the early-out path in
    SPIRVLowerBitCastToNonStandardType.cpp. As a consequence, if the pass
    has vector with non-standard size as input but with only
    `SPV_EXT_long_vector` enabled, it will crash later.
    
    Also added missing coverage in SPIRVUtils.cpp when handling intrinsics.
    
    The new tests simply mirror the ones in
    `extensions/INTEL/SPV_INTEL_vector_compute`.

--- lib/SPIRV/SPIRVUtil.cpp
+++ lib/SPIRV/SPIRVUtil.cpp
@@ -2042,6 +2042,7 @@ bool checkTypeForSPIRVExtendedInstLowering(IntrinsicInst *II, SPIRVModule *BM) {
       return true;
     if ((!Ty->isFloatTy() && !Ty->isDoubleTy() && !Ty->isHalfTy()) ||
         (!BM->hasCapability(CapabilityVectorAnyINTEL) &&
+         !BM->hasCapability(CapabilityLongVectorEXT) &&
          ((NumElems > 4) && (NumElems != 8) && (NumElems != 16)))) {
       BM->SPIRVCK(
           false, InvalidFunctionCall, II->getCalledOperand()->getName().str());
@@ -2061,6 +2062,7 @@ bool checkTypeForSPIRVExtendedInstLowering(IntrinsicInst *II, SPIRVModule *BM) {
       return true;
     if ((!Ty->isIntegerTy()) ||
         (!BM->hasCapability(CapabilityVectorAnyINTEL) &&
+         !BM->hasCapability(CapabilityLongVectorEXT) &&
          ((NumElems > 4) && (NumElems != 8) && (NumElems != 16)))) {
       BM->SPIRVCK(
           false, InvalidFunctionCall, II->getCalledOperand()->getName().str());
