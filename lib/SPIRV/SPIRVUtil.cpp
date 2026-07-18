1e3a2aecea62c3831d0512e651daada3bb21fc17
Qi Ye <qi.ye@intel.com>
2026-07-17 21:19:32 -0400

    [Backport to 21] Add missing coverage for SPV_EXT_long_vector (#3890)
    Backport of #3869

--- lib/SPIRV/SPIRVUtil.cpp
+++ lib/SPIRV/SPIRVUtil.cpp
@@ -1950,6 +1950,7 @@ bool checkTypeForSPIRVExtendedInstLowering(IntrinsicInst *II, SPIRVModule *BM) {
       return true;
     if ((!Ty->isFloatTy() && !Ty->isDoubleTy() && !Ty->isHalfTy()) ||
         (!BM->hasCapability(CapabilityVectorAnyINTEL) &&
+         !BM->hasCapability(CapabilityLongVectorEXT) &&
          ((NumElems > 4) && (NumElems != 8) && (NumElems != 16)))) {
       BM->SPIRVCK(false, InvalidFunctionCall,
                   II->getCalledOperand()->getName().str());
@@ -1969,6 +1970,7 @@ bool checkTypeForSPIRVExtendedInstLowering(IntrinsicInst *II, SPIRVModule *BM) {
       return true;
     if ((!Ty->isIntegerTy()) ||
         (!BM->hasCapability(CapabilityVectorAnyINTEL) &&
+         !BM->hasCapability(CapabilityLongVectorEXT) &&
          ((NumElems > 4) && (NumElems != 8) && (NumElems != 16)))) {
       BM->SPIRVCK(false, InvalidFunctionCall,
                   II->getCalledOperand()->getName().str());
