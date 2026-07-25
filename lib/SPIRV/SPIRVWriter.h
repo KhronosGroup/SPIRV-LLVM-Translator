38f497153418a4936a8a6b7edc63024e8c0e9960
Aiden Grossman <aidengrossman@google.com>
2026-07-25 06:04:32 -0700

    Prefer RequiredPassInfoMixin
    In LLVM 23, {Optional,Required}PassInfoMixin became preferred for
    creating passes instead of raw PassInfoMixin with isRequired. In LLVM
    24, PassInfoMixin will be moved to a detail namespace, causing
    compilation failures.

--- lib/SPIRV/SPIRVWriter.h
+++ lib/SPIRV/SPIRVWriter.h
@@ -270,7 +270,7 @@ private:
                                                    Function *F);
 };
 
-class LLVMToSPIRVPass : public PassInfoMixin<LLVMToSPIRVPass> {
+class LLVMToSPIRVPass : public RequiredPassInfoMixin<LLVMToSPIRVPass> {
 public:
   LLVMToSPIRVPass(SPIRVModule *SMod) : SMod(SMod) {}
 
@@ -281,8 +281,6 @@ public:
     return PassInstance.runLLVMToSPIRV(M) ? llvm::PreservedAnalyses::none()
                                           : llvm::PreservedAnalyses::all();
   }
-
-  static bool isRequired() { return true; }
 
 private:
   SPIRVModule *SMod;
